0805b1d6a83dcdad95397dc070c809414e8bad83
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-12-24 04:38:55 +0000

    * Missing files. Sorry!

---
+++ Include/HadesMem/Common.h
@@ -0,0 +1,30 @@
+/*
+This file is part of HadesMem.
+Copyright © 2010 RaptorFactor (aka Cypherjb, Cypher, Chazwazza). 
+<http://www.raptorfactor.com/> <raptorfactor@raptorfactor.com>
+
+HadesMem is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HadesMem is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Common
+#include "../Common/Common/EnsureCleanup.h"
+#include "../Common/Common/Error.h"
+#include "../Common/Common/Filesystem.h"
+#include "../Common/Common/I18n.h"
+#include "../Common/Common/Logger.h"
+#include "../Common/Common/StringBuffer.h"
+#include "../Common/Common/VecN.h"
+#include "../Common/Common/WinAux.h"
