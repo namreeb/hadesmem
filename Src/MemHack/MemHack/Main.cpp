e9aa851825271e7e57c4aa4beb039c9fc43c0de7
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-01-25 17:49:38 +0000

    * Placeholder. This should be an entertaining test.... Fuck I hate native GUI programming.

---
+++ Src/MemHack/MemHack/Main.cpp
@@ -0,0 +1,13 @@
+// Windows API
+#include <Windows.h>
+
+// Entry point
+int CALLBACK WinMain(
+  __in  HINSTANCE /*hInstance*/,
+  __in  HINSTANCE /*hPrevInstance*/,
+  __in  LPSTR /*lpCmdLine*/,
+  __in  int /*nCmdShow*/
+)
+{
+  return 0;
+}
