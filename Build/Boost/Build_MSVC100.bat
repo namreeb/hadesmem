a6eee950e6bcd6df15bbb15ecd05ed8092d5fd24
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-03-30 17:46:01 +0000

    * Tweak Boost build script to only build the necessary variants. * Project file tweaks (DLL -> LIB for AsmJit and BeaEngine, Static link python for MemSandbox and PyHadesMem).

--- Build/Boost/Build_MSVC100.bat
+++ Build/Boost/Build_MSVC100.bat
@@ -1,7 +1,7 @@
 cd ..\..\Src\Dependencies\Boost
-bjam --toolset=msvc-10.0 --build-type=complete -j 4 address-model=32 stage > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
+bjam --toolset=msvc-10.0 -j 4 threading=multi link=static runtime-link=shared address-model=32 debug release stage > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
 ren stage\lib ia32
-bjam --toolset=msvc-10.0 --build-type=complete -j 4 address-model=64 stage > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
+bjam --toolset=msvc-10.0 -j 4 threading=multi link=static runtime-link=shared address-model=64 debug release stage > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
 ren stage\lib amd64
 mkdir stage\lib
 move stage\amd64 stage\lib
