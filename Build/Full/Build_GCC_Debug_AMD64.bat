511bb78a6717c0d70cee0ba3a536a1530948a5b6
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-04-23 06:25:21 +0000

    * A few more test scripts. Not yet working.

---
+++ Build/Full/Build_GCC_Debug_AMD64.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+bjam --build-scripting --run-tests -j 4 toolset=gcc address-model=64 debug > Build\Full\Build_GCC_Debug_AMD64.txt
---
+++ Build/Full/Build_GCC_Release_IA32.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+bjam --build-scripting --run-tests -j 4 toolset=gcc address-model=32 release > Build\Full\Build_GCC_Release_IA32.txt
