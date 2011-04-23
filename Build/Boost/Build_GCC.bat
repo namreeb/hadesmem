fc6fc138d0443db4552c1bf88a6834529a08572d
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-04-23 15:54:39 +0000

    * Remove GCC build scripts for now as they're useless and I was just using them for some quick tests.

--- Build/Boost/Build_GCC.bat
+++
@@ -1,9 +0,0 @@
-cd ..\..\Src\Dependencies\Boost
-call bootstrap.bat
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
-ren stage\lib gcc_ia32
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
-ren stage\lib gcc_amd64
-mkdir stage\lib
-move stage\gcc_amd64 stage\lib
-move stage\gcc_ia32 stage\lib
--- Build/Full/Build_GCC_Release_AMD64.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-bjam --build-scripting --run-tests -j 4 toolset=gcc address-model=64 release > Build\Full\Build_GCC_Release_AMD64.txt
