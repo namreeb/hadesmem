1f97d6f6403828b503826a977dcf9c27007a3a3e
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-05-11 08:48:22 +0000

    * Enable c++0x.

--- Build/Boost/Build_GCC.bat
+++ Build/Boost/Build_GCC.bat
@@ -1,8 +1,8 @@
 cd ..\..\Src\Dependencies\Boost
 call bootstrap.bat
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 debug release stage > ..\..\..\Build\Boost\Build_GCC_IA32.txt
+bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release stage > ..\..\..\Build\Boost\Build_GCC_IA32.txt
 ren stage\lib gcc_ia32
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 debug release stage > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
+bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release stage > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
 ren stage\lib gcc_amd64
 mkdir stage\lib
 move stage\gcc_amd64 stage\lib
--- Build/Boost/Test_GCC.bat
+++ Build/Boost/Test_GCC.bat
@@ -1,3 +1,3 @@
 cd ..\..\Src\Dependencies\Boost\status
-"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 > ..\..\..\..\Build\Boost\Test_GCC_IA32.txt
-"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 > ..\..\..\..\Build\Boost\Test_GCC_AMD64.txt
+"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x > ..\..\..\..\Build\Boost\Test_GCC_IA32.txt
+"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x > ..\..\..\..\Build\Boost\Test_GCC_AMD64.txt
