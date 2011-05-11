7c8d07a97b001f4aea75af3d36ce0c502c91dccc
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-05-11 08:11:34 +0000

    * Config helper header.

---
+++ Include/Common/HadesCommon/Config.hpp
@@ -0,0 +1,35 @@
+/*
+This file is part of HadesMem.
+Copyright (C) 2011 Joshua Boyce (a.k.a. RaptorFactor).
+<http://www.raptorfactor.com/> <raptorfactor@raptorfactor.com>
+
+HadesMem is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HadesMem is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Boost
+#include <boost/config.hpp>
+
+#if defined(BOOST_MSVC)
+#define HADES_MSVC
+#elif defined(BOOST_INTEL)
+#define HADES_INTEL
+#elif defined(BOOST_CLANG)
+#define HADES_CLANG
+// Detect GCC
+// Note: This check must be last as both ICC and Clang also define __GNUC__ 
+#elif defined(__GNUC__)
+#define HADES_GCC
+#endif
