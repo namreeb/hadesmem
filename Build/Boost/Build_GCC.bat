dcf90b3d6ba117441a8e6ea835d9b0e654ffedca
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-05-15 07:42:27 +0000

    * Minor build script changes. * Todo.

--- Build/Boost/Build_GCC.bat
+++ Build/Boost/Build_GCC.bat
@@ -1,8 +1,3 @@
 cd ..\..\Src\Dependencies\Boost
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release stage > ..\..\..\Build\Boost\Build_GCC_IA32.txt
-ren stage\lib gcc_ia32
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release stage > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
-ren stage\lib gcc_amd64
-mkdir stage\lib
-move stage\gcc_amd64 stage\lib
-move stage\gcc_ia32 stage\lib
+bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
+bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
--- Build/Boost/Build_MSVC100.bat
+++ Build/Boost/Build_MSVC100.bat
@@ -1,8 +1,3 @@
 cd ..\..\Src\Dependencies\Boost
-bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=32 debug release stage > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
-ren stage\lib msvc_ia32
-bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=64 debug release stage > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
-ren stage\lib msvc_amd64
-mkdir stage\lib
-move stage\msvc_amd64 stage\lib
-move stage\msvc_ia32 stage\lib
+bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=32 debug release > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
+bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=64 debug release > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
