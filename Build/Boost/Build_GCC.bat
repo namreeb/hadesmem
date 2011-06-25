70ef6c0daa0fb43f8bdf543256c9612820805f06
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-06-25 17:44:37 +0000

    * [Build] [Boost] Fix scripts to add binaries/libs for each configuration to a separate stage dir. * [Todo] Add temp file (generated by MS Office when you're editing the file) to ignore list.

--- Build/Boost/Build_GCC.bat
+++ Build/Boost/Build_GCC.bat
@@ -1,3 +1,3 @@
 cd ..\..\Src\Dependencies\Boost
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
-bjam -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
+bjam -j 4 --stagedir=gcc-x86 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
+bjam -j 4 --stagedir=gcc-x64 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
--- Build/Boost/Build_MSVC100.bat
+++ Build/Boost/Build_MSVC100.bat
@@ -1,3 +1,3 @@
 cd ..\..\Src\Dependencies\Boost
-bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=32 debug release > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
-bjam -j 4 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=64 debug release > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
+bjam -j 4 --stagedir=msvc-x86 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=32 debug release > ..\..\..\Build\Boost\Build_MSVC100_IA32.txt
+bjam -j 4 --stagedir=msvc-x64 toolset=msvc-10.0 threading=multi link=static runtime-link=shared address-model=64 debug release > ..\..\..\Build\Boost\Build_MSVC100_AMD64.txt
