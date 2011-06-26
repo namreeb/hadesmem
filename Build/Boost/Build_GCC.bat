36cffeea160ac768071a573c916ee1214ee0d366
theraptorfactor <theraptorfactor@cdac023c-1092-11df-8e59-a5c54279f3f5>
2011-06-26 13:09:20 +0000

    * [Dependencies/Boost] Change build scripts to build as shared libraries. * [Dependencies/AsmJit] Build as shared library. * [Dependencies/BeaEngine] Build as shared library. * [Misc] Delete some unused/unneeded files. * [Other] Minor cleanup.

--- Build/Boost/Build_GCC.bat
+++ Build/Boost/Build_GCC.bat
@@ -1,3 +1,3 @@
 cd ..\..\Src\Dependencies\Boost
-bjam -j 4 --stagedir=stage/gcc-x86 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
-bjam -j 4 --stagedir=stage/gcc-x64 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
+bjam -j 4 --stagedir=stage/gcc-x86 toolset=gcc threading=multi link=shared runtime-link=shared address-model=32 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_IA32.txt
+bjam -j 4 --stagedir=stage/gcc-x64 toolset=gcc threading=multi link=shared runtime-link=shared address-model=64 cxxflags=-std=c++0x debug release > ..\..\..\Build\Boost\Build_GCC_AMD64.txt
--- Build/Boost/Test_GCC.bat
+++
@@ -1,3 +0,0 @@
-cd ..\..\Src\Dependencies\Boost\status
-"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=32 cxxflags=-std=c++0x > ..\..\..\..\Build\Boost\Test_GCC_IA32.txt
-"../bjam.exe" -j 4 toolset=gcc threading=multi link=static runtime-link=shared address-model=64 cxxflags=-std=c++0x > ..\..\..\..\Build\Boost\Test_GCC_AMD64.txt
