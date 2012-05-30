964d37f89cdd485570bf1fe703d9851456e82ce9
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-05-30 18:30:04 +0000

    * Minor rearchitect.

--- include/memory/hadesmemory/process.hpp
+++ include/memory/hadesmemory/process.hpp
@@ -12,10 +12,7 @@ namespace hadesmem
 class Process
 {
 public:
-  class Error : public virtual HadesMemError
-  { };
-  
-  explicit Process(DWORD proc_id);
+  explicit Process(DWORD id);
   
   Process(Process&& other);
   
@@ -27,25 +24,26 @@ public:
   
   HANDLE GetHandle() const;
   
-  std::string GetPath() const;
-  
-  bool IsWoW64() const;
-  
 private:
   Process(Process const&);
   Process& operator=(Process const&);
   
-  void SetWoW64();
+  void CheckWoW64() const;
+  
+  HANDLE Open(DWORD id);
   
   void Cleanup();
   
   DWORD id_;
   HANDLE handle_;
-  bool is_wow64_;
 };
 
 bool operator==(Process const& lhs, Process const& rhs);
 
 bool operator!=(Process const& lhs, Process const& rhs);
+
+std::string GetPath(Process const& process);
+
+bool IsWoW64(Process const& process);
 
 }
--- src/memory/memory/process.cpp
+++ src/memory/memory/process.cpp
@@ -25,36 +25,17 @@ namespace hadesmem
 
 Process::Process(DWORD id)
   : id_(id), 
-  handle_(nullptr), 
-  is_wow64_(false)
+  handle_(Open(id))
 {
-  handle_ = OpenProcess(PROCESS_CREATE_THREAD | 
-    PROCESS_QUERY_INFORMATION | 
-    PROCESS_VM_OPERATION | 
-    PROCESS_VM_READ | 
-    PROCESS_VM_WRITE, 
-    FALSE, 
-    id_);
-  if (!handle_)
-  {
-    DWORD const last_error = GetLastError();
-    BOOST_THROW_EXCEPTION(Error() << 
-      ErrorFunction("Process::Process") << 
-      ErrorString("OpenProcess failed.") << 
-      ErrorCodeWinLast(last_error));
-  }
-  
-  SetWoW64();
+  CheckWoW64();
 }
 
 Process::Process(Process&& other)
   : id_(other.id_), 
-  handle_(other.handle_), 
-  is_wow64_(other.is_wow64_)
+  handle_(other.handle_)
 {
   other.id_ = 0;
   other.handle_ = nullptr;
-  other.is_wow64_ = false;
 }
 
 Process& Process::operator=(Process&& other)
@@ -63,7 +44,6 @@ Process& Process::operator=(Process&& other)
   
   std::swap(this->id_, other.id_);
   std::swap(this->handle_, other.handle_);
-  std::swap(this->is_wow64_, other.is_wow64_);
   
   return *this;
 }
@@ -83,35 +63,14 @@ HANDLE Process::GetHandle() const
   return handle_;
 }
 
-std::string Process::GetPath() const
-{
-  std::array<wchar_t, MAX_PATH> path = { { } };
-  DWORD path_len = static_cast<DWORD>(path.size());
-  if (!QueryFullProcessImageName(handle_, 0, path.data(), &path_len))
-  {
-      DWORD const last_error = GetLastError();
-      BOOST_THROW_EXCEPTION(Error() << 
-        ErrorFunction("Process::GetPath") << 
-        ErrorString("QueryFullProcessImageName failed.") << 
-        ErrorCodeWinLast(last_error));
-  }
-  
-  return boost::locale::conv::utf_to_utf<char>(path.data());
-}
-
-bool Process::IsWoW64() const
-{
-  return is_wow64_;
-}
-
-void Process::SetWoW64()
+void Process::CheckWoW64() const
 {
   BOOL is_wow64_me = FALSE;
   if (!IsWow64Process(GetCurrentProcess(), &is_wow64_me))
   {
     DWORD const last_error = GetLastError();
-    BOOST_THROW_EXCEPTION(Error() << 
-      ErrorFunction("Process::SetWoW64") << 
+    BOOST_THROW_EXCEPTION(HadesMemError() << 
+      ErrorFunction("Process::CheckWoW64") << 
       ErrorString("Could not detect WoW64 status of current process.") << 
       ErrorCodeWinLast(last_error));
   }
@@ -120,21 +79,40 @@ void Process::SetWoW64()
   if (!IsWow64Process(handle_, &is_wow64))
   {
     DWORD const last_error = GetLastError();
-    BOOST_THROW_EXCEPTION(Error() << 
-      ErrorFunction("Process::SetWoW64") << 
+    BOOST_THROW_EXCEPTION(HadesMemError() << 
+      ErrorFunction("Process::CheckWoW64") << 
       ErrorString("Could not detect WoW64 status of target process.") << 
       ErrorCodeWinLast(last_error));
   }
   
-  is_wow64_ = (is_wow64 != FALSE);
-  
   if (is_wow64_me != is_wow64)
   {
-    BOOST_THROW_EXCEPTION(Error() << 
-      ErrorFunction("Process::SetWoW64") << 
+    BOOST_THROW_EXCEPTION(HadesMemError() << 
+      ErrorFunction("Process::CheckWoW64") << 
       ErrorString("Cross-architecture process manipulation is currently "
         "unsupported."));
   }
+}
+
+HANDLE Process::Open(DWORD id)
+{
+  HANDLE handle = OpenProcess(PROCESS_CREATE_THREAD | 
+    PROCESS_QUERY_INFORMATION | 
+    PROCESS_VM_OPERATION | 
+    PROCESS_VM_READ | 
+    PROCESS_VM_WRITE, 
+    FALSE, 
+    id);
+  if (!handle)
+  {
+    DWORD const last_error = GetLastError();
+    BOOST_THROW_EXCEPTION(HadesMemError() << 
+      ErrorFunction("Process::Open") << 
+      ErrorString("OpenProcess failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+  
+  return handle;
 }
 
 void Process::Cleanup()
@@ -144,7 +122,7 @@ void Process::Cleanup()
     if (!CloseHandle(handle_))
     {
       DWORD const last_error = GetLastError();
-      BOOST_THROW_EXCEPTION(Error() << 
+      BOOST_THROW_EXCEPTION(HadesMemError() << 
         ErrorFunction("Process::Cleanup") << 
         ErrorString("CloseHandle failed.") << 
         ErrorCodeWinLast(last_error));
@@ -153,7 +131,6 @@ void Process::Cleanup()
   
   id_ = 0;
   handle_ = nullptr;
-  is_wow64_ = false;
 }
 
 bool operator==(Process const& lhs, Process const& rhs)
@@ -164,6 +141,38 @@ bool operator==(Process const& lhs, Process const& rhs)
 bool operator!=(Process const& lhs, Process const& rhs)
 {
   return !(lhs == rhs);
+}
+
+std::string GetPath(Process const& process)
+{
+  std::array<wchar_t, MAX_PATH> path = { { } };
+  DWORD path_len = static_cast<DWORD>(path.size());
+  if (!QueryFullProcessImageName(process.GetHandle(), 0, path.data(), 
+    &path_len))
+  {
+      DWORD const last_error = GetLastError();
+      BOOST_THROW_EXCEPTION(HadesMemError() << 
+        ErrorFunction("GetPath") << 
+        ErrorString("QueryFullProcessImageName failed.") << 
+        ErrorCodeWinLast(last_error));
+  }
+  
+  return boost::locale::conv::utf_to_utf<char>(path.data());
+}
+
+bool IsWoW64(Process const& process)
+{
+  BOOL is_wow64 = FALSE;
+  if (!IsWow64Process(process.GetHandle(), &is_wow64))
+  {
+    DWORD const last_error = GetLastError();
+    BOOST_THROW_EXCEPTION(HadesMemError() << 
+      ErrorFunction("IsWoW64") << 
+      ErrorString("IsWow64Process failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+  
+  return is_wow64 != FALSE;
 }
 
 }
