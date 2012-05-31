fb90e3d59f7f945601456654e2783aea00c0898c
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-05-31 04:35:14 +0000

    * Licensing nonsense.

--- tests/process.cpp
+++ tests/process.cpp
@@ -1,3 +1,10 @@
+// Copyright Joshua Boyce 2010-2012.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// This file is part of HadesMem.
+// <http://www.raptorfactor.com/> <raptorfactor@raptorfactor.com>
+
 #include "hadesmem/process.hpp"
 
 #define BOOST_TEST_MODULE process
