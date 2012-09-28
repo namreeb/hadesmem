3e5d261c164e17a7ef7332d5157303a38a614ece
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-09-28 05:17:35 +0000

    * Added another Region test.

--- tests/region.cpp
+++ tests/region.cpp
@@ -44,6 +44,19 @@ BOOST_AUTO_TEST_CASE(region)
   
   BOOST_CHECK_NE(first_region.GetSize(), 0U);
   BOOST_CHECK_NE(first_region.GetState(), 0U);
+
+  hadesmem::Region const first_region_other(&process, static_cast<PBYTE>(
+    first_region.GetBase()) + 1);
+  BOOST_CHECK_EQUAL(first_region.GetBase(), first_region_other.GetBase());
+  BOOST_CHECK_EQUAL(first_region.GetAllocBase(), 
+    first_region_other.GetAllocBase());
+  BOOST_CHECK_EQUAL(first_region.GetAllocProtect(), 
+    first_region_other.GetAllocProtect());
+  BOOST_CHECK_EQUAL(first_region.GetSize(), first_region_other.GetSize());
+  BOOST_CHECK_EQUAL(first_region.GetState(), first_region_other.GetState());
+  BOOST_CHECK_EQUAL(first_region.GetProtect(), 
+    first_region_other.GetProtect());
+  BOOST_CHECK_EQUAL(first_region.GetType(), first_region_other.GetType());
   
   hadesmem::Region const second_region(&process, static_cast<char const* const>(
     first_region.GetBase()) + first_region.GetSize());
