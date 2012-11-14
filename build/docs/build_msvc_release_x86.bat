ed3b810dfb23bd27e24ec6fc202ccc56b76afb03
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-11-14 23:33:58 +0000

    * Remove old docs stuff as it will all be replaced with doxygen style docs.

--- build/docs/build_msvc_release_x86.bat
+++
@@ -1,7 +0,0 @@
-set OLDCD=%CD%
-pushd ..\..\
-set OLDPATH=%PATH%
-set PATH=%BOOST_ROOT%;%PATH%
-b2 docs -j %NUMBER_OF_PROCESSORS% toolset=msvc address-model=32 release > %OLDCD%\msvc_release_x86.txt
-set PATH=%OLDPATH%
-popd
