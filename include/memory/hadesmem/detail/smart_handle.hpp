b4fa1ad87ff88d152df5dd859eba99dea24d96c0
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-11-28 11:18:31 +0000

    * Move smart handle implementation to cpp file. * Todo.

--- include/memory/hadesmem/detail/smart_handle.hpp
+++ include/memory/hadesmem/detail/smart_handle.hpp
@@ -3,11 +3,8 @@
 
 #pragma once
 
-#include <cassert>
-
 #include <windows.h>
 
-#include "hadesmem/error.hpp"
 #include "hadesmem/config.hpp"
 
 namespace hadesmem
@@ -20,97 +17,29 @@ class SmartHandle
 {
 public:
   explicit SmartHandle(HANDLE handle = nullptr, HANDLE invalid = nullptr) 
-    HADESMEM_NOEXCEPT
-    : handle_(handle), 
-    invalid_(invalid)
-  { }
+    HADESMEM_NOEXCEPT;
 
-  SmartHandle& operator=(HANDLE handle) HADESMEM_NOEXCEPT
-  {
-    CleanupUnchecked();
+  SmartHandle& operator=(HANDLE handle) HADESMEM_NOEXCEPT;
 
-    handle_ = handle;
+  SmartHandle(SmartHandle&& other) HADESMEM_NOEXCEPT;
 
-    return *this;
-  }
+  SmartHandle& operator=(SmartHandle&& other) HADESMEM_NOEXCEPT;
 
-  SmartHandle(SmartHandle&& other) HADESMEM_NOEXCEPT
-    : handle_(other.handle_), 
-    invalid_(other.invalid_)
-  {
-    other.handle_ = nullptr;
-  }
+  ~SmartHandle();
 
-  SmartHandle& operator=(SmartHandle&& other) HADESMEM_NOEXCEPT
-  {
-    CleanupUnchecked();
+  HANDLE GetHandle() const HADESMEM_NOEXCEPT;
 
-    handle_ = other.handle_;
-    other.handle_ = other.invalid_;
+  HANDLE GetInvalid() const HADESMEM_NOEXCEPT;
 
-    invalid_ = other.invalid_;
+  bool IsValid() const HADESMEM_NOEXCEPT;
 
-    return *this;
-  }
-
-  ~SmartHandle()
-  {
-    CleanupUnchecked();
-  }
-
-  HANDLE GetHandle() const HADESMEM_NOEXCEPT
-  {
-    return handle_;
-  }
-
-  HANDLE GetInvalid() const HADESMEM_NOEXCEPT
-  {
-    return invalid_;
-  }
-
-  bool IsValid() const HADESMEM_NOEXCEPT
-  {
-    return GetHandle() != GetInvalid();
-  }
-
-  void Cleanup()
-  {
-    if (handle_ == invalid_)
-    {
-      return;
-    }
-
-    if (!::CloseHandle(handle_))
-    {
-      DWORD const last_error = ::GetLastError();
-      HADESMEM_THROW_EXCEPTION(Error() << 
-        ErrorString("CloseHandle failed.") << 
-        ErrorCodeWinLast(last_error));
-    }
-
-    handle_ = invalid_;
-  }
+  void Cleanup();
 
 private:
   SmartHandle(SmartHandle const& other) HADESMEM_DELETED_FUNCTION;
   SmartHandle& operator=(SmartHandle const& other) HADESMEM_DELETED_FUNCTION;
 
-  void CleanupUnchecked() HADESMEM_NOEXCEPT
-  {
-    try
-    {
-      Cleanup();
-    }
-    catch (std::exception const& e)
-    {
-      (void)e;
-
-      // WARNING: Handle is leaked if 'Cleanup' fails.
-      assert(boost::diagnostic_information(e).c_str() && false);
-      
-      handle_ = invalid_;
-    }
-  }
+  void CleanupUnchecked() HADESMEM_NOEXCEPT;
 
   HANDLE handle_;
   HANDLE invalid_;
---
+++ src/memory/memory/detail/smart_handle.cpp
@@ -0,0 +1,106 @@
+// Copyright (C) 2010-2012 Joshua Boyce.
+// See the file COPYING for copying permission.
+
+#include "hadesmem/detail/smart_handle.hpp"
+
+#include <cassert>
+
+#include "hadesmem/error.hpp"
+
+namespace hadesmem
+{
+
+namespace detail
+{
+
+SmartHandle::SmartHandle(HANDLE handle, HANDLE invalid) HADESMEM_NOEXCEPT
+  : handle_(handle), 
+  invalid_(invalid)
+{ }
+
+SmartHandle& SmartHandle::operator=(HANDLE handle) HADESMEM_NOEXCEPT
+{
+  CleanupUnchecked();
+
+  handle_ = handle;
+
+  return *this;
+}
+
+SmartHandle::SmartHandle(SmartHandle&& other) HADESMEM_NOEXCEPT
+  : handle_(other.handle_), 
+  invalid_(other.invalid_)
+{
+  other.handle_ = nullptr;
+}
+
+SmartHandle& SmartHandle::operator=(SmartHandle&& other) HADESMEM_NOEXCEPT
+{
+  CleanupUnchecked();
+
+  handle_ = other.handle_;
+  other.handle_ = other.invalid_;
+
+  invalid_ = other.invalid_;
+
+  return *this;
+}
+
+SmartHandle::~SmartHandle()
+{
+  CleanupUnchecked();
+}
+
+HANDLE SmartHandle::GetHandle() const HADESMEM_NOEXCEPT
+{
+  return handle_;
+}
+
+HANDLE SmartHandle::GetInvalid() const HADESMEM_NOEXCEPT
+{
+  return invalid_;
+}
+
+bool SmartHandle::IsValid() const HADESMEM_NOEXCEPT
+{
+  return GetHandle() != GetInvalid();
+}
+
+void SmartHandle::Cleanup()
+{
+  if (handle_ == invalid_)
+  {
+    return;
+  }
+
+  if (!::CloseHandle(handle_))
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("CloseHandle failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  handle_ = invalid_;
+}
+
+void SmartHandle::CleanupUnchecked() HADESMEM_NOEXCEPT
+{
+  try
+  {
+    Cleanup();
+  }
+  catch (std::exception const& e)
+  {
+    (void)e;
+
+    // WARNING: Handle is leaked if 'Cleanup' fails.
+    assert(boost::diagnostic_information(e).c_str() && false);
+
+    handle_ = invalid_;
+  }
+}
+
+}
+
+}
