bb8345992a9e177b3fd9f7e23714ed0d18da1795
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2012-12-31 09:09:32 +0000

    * Change build scripts to not compile Boost.Python.

--- build/boost/clang32.bat
+++ build/boost/clang32.bat
@@ -2,6 +2,6 @@ set OLDCD=%CD%
 pushd %BOOST_ROOT%
 set OLDPATH=%PATH%
 set PATH=%MINGW32_CLANG%;%MINGW32_CLANG%\bin\;%PATH%
-b2 -j %NUMBER_OF_PROCESSORS% toolset=clang cxxflags=-std=c++11 address-model=32 architecture=x86 --stagedir=stage/clang-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA stage > %OLDCD%\clang32.txt 2>&1
+b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=clang cxxflags=-std=c++11 address-model=32 architecture=x86 --stagedir=stage/clang-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA stage > %OLDCD%\clang32.txt 2>&1
 set PATH=%OLDPATH%
 popd
--- build/boost/gcc32.bat
+++ build/boost/gcc32.bat
@@ -2,6 +2,6 @@ set OLDCD=%CD%
 pushd %BOOST_ROOT%
 set OLDPATH=%PATH%
 set PATH=%MINGW32%;%MINGW32%\bin\;%PATH%
-b2 -j %NUMBER_OF_PROCESSORS% toolset=gcc cxxflags="-std=c++11 -march=pentium4" address-model=32 architecture=x86 --stagedir=stage/gcc-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA stage > %OLDCD%\gcc32.txt 2>&1
+b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=gcc cxxflags="-std=c++11 -march=pentium4" address-model=32 architecture=x86 --stagedir=stage/gcc-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA stage > %OLDCD%\gcc32.txt 2>&1
 set PATH=%OLDPATH%
 popd
