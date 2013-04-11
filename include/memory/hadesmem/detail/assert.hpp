af8a6e60273484d5a6f2e80a03acd30d3197c8a2
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-04-11 07:19:47 +0000

    * Add missing file from previous commit.

---
+++ include/memory/hadesmem/detail/assert.hpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2010-2013 Joshua Boyce.
+// See the file COPYING for copying permission.
+
+#pragma once
+
+#include <hadesmem/detail/warning_disable_prefix.hpp>
+#include <boost/assert.hpp>
+#include <hadesmem/detail/warning_disable_suffix.hpp>
+
+#define HADESMEM_ASSERT(...) BOOST_ASSERT(__VA_ARGS__)
