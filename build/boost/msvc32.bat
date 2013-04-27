63f94c68bc9bd90ea219d8fec1c953cf2757e62e
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-04-27 09:10:48 +0000

    * Add BOOST_USE_WINDOWS_H to match Jamroot.

--- build/boost/msvc32.bat
+++ build/boost/msvc32.bat
@@ -1,4 +1,4 @@
 set OLDCD=%CD%
 pushd %BOOST_ROOT%
-b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=msvc address-model=32 architecture=x86 --stagedir=stage/msvc-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA stage > %OLDCD%\msvc32.txt 2>&1
+b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=msvc address-model=32 architecture=x86 --stagedir=stage/msvc-x86 link=static runtime-link=static threading=multi debug-symbols=on define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA define=BOOST_USE_WINDOWS_H stage > %OLDCD%\msvc32.txt 2>&1
 popd
