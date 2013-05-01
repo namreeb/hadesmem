523341be075995e62a24610dd78ddcbc6bd0e92e
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-01 13:11:04 +0000

    * Make ProcessIterator more inline. * Remove PImpl from ProcessList. * Make ProcessList more inline.

--- include/memory/hadesmem/process_list.hpp
+++ include/memory/hadesmem/process_list.hpp
@@ -4,9 +4,17 @@
 #pragma once
 
 #include <memory>
+#include <utility>
 #include <iterator>
 
+#include <hadesmem/detail/warning_disable_prefix.hpp>
+#include <boost/optional.hpp>
+#include <hadesmem/detail/warning_disable_suffix.hpp>
+
 #include <hadesmem/config.hpp>
+#include <hadesmem/detail/assert.hpp>
+#include <hadesmem/process_entry.hpp>
+#include <hadesmem/detail/smart_handle.hpp>
 
 namespace hadesmem
 {
@@ -21,36 +29,82 @@ class ProcessIterator : public std::iterator<std::input_iterator_tag,
   ProcessEntry>
 {
 public:
-  ProcessIterator() HADESMEM_NOEXCEPT;
+  ProcessIterator() HADESMEM_NOEXCEPT
+    : impl_()
+  { }
 
   explicit ProcessIterator(int dummy);
 
-  ProcessIterator(ProcessIterator const& other) HADESMEM_NOEXCEPT;
+  ProcessIterator(ProcessIterator const& other) HADESMEM_NOEXCEPT
+    : impl_(other.impl_)
+  { }
 
-  ProcessIterator& operator=(ProcessIterator const& other) HADESMEM_NOEXCEPT;
+  ProcessIterator& operator=(ProcessIterator const& other) HADESMEM_NOEXCEPT
+  {
+    impl_ = other.impl_;
 
-  ProcessIterator(ProcessIterator&& other) HADESMEM_NOEXCEPT;
+    return *this;
+  }
 
-  ProcessIterator& operator=(ProcessIterator&& other) HADESMEM_NOEXCEPT;
+  ProcessIterator(ProcessIterator&& other) HADESMEM_NOEXCEPT
+    : impl_(std::move(other.impl_))
+  { }
 
-  ~ProcessIterator();
+  ProcessIterator& operator=(ProcessIterator&& other) HADESMEM_NOEXCEPT
+  {
+    impl_ = std::move(other.impl_);
+
+    return *this;
+  }
+
+  ~ProcessIterator() HADESMEM_NOEXCEPT
+  { }
   
-  reference operator*() const HADESMEM_NOEXCEPT;
+  reference operator*() const HADESMEM_NOEXCEPT
+  {
+    HADESMEM_ASSERT(impl_.get());
+    return *impl_->process_;
+  }
   
-  pointer operator->() const HADESMEM_NOEXCEPT;
+  pointer operator->() const HADESMEM_NOEXCEPT
+  {
+    HADESMEM_ASSERT(impl_.get());
+    return &*impl_->process_;
+  }
   
   ProcessIterator& operator++();
   
-  ProcessIterator operator++(int);
+  ProcessIterator operator++(int)
+  {
+    ProcessIterator iter(*this);
+    ++*this;
+    return iter;
+  }
   
-  bool operator==(ProcessIterator const& other) const HADESMEM_NOEXCEPT;
+  bool operator==(ProcessIterator const& other) const HADESMEM_NOEXCEPT
+  {
+    return impl_ == other.impl_;
+  }
   
-  bool operator!=(ProcessIterator const& other) const HADESMEM_NOEXCEPT;
+  bool operator!=(ProcessIterator const& other) const HADESMEM_NOEXCEPT
+  {
+    return !(*this == other);
+  }
   
 private:
+  struct Impl
+  {
+    explicit Impl() HADESMEM_NOEXCEPT
+      : snap_(INVALID_HANDLE_VALUE, INVALID_HANDLE_VALUE), 
+      process_()
+    { }
+  
+    detail::SmartHandle snap_;
+    boost::optional<ProcessEntry> process_;
+  };
+
   // Using a shared_ptr to provide shallow copy semantics, as 
   // required by InputIterator.
-  struct Impl;
   std::shared_ptr<Impl> impl_;
 };
 
@@ -60,15 +114,28 @@ public:
   typedef ProcessIterator iterator;
   typedef ProcessIterator const_iterator;
 
-  ProcessList() HADESMEM_NOEXCEPT;
-  
-  iterator begin();
-  
-  const_iterator begin() const;
-  
-  iterator end() HADESMEM_NOEXCEPT;
-  
-  const_iterator end() const HADESMEM_NOEXCEPT;
+  HADESMEM_CONSTEXPR ProcessList() HADESMEM_NOEXCEPT
+  { }
+
+  iterator begin()
+  {
+    return iterator(0);
+  }
+
+  const_iterator begin() const
+  {
+    return iterator(0);
+  }
+
+  iterator end() HADESMEM_NOEXCEPT
+  {
+    return iterator();
+  }
+
+  const_iterator end() const HADESMEM_NOEXCEPT
+  {
+    return iterator();
+  }
 };
 
 }
--- src/memory/memory/process_list.cpp
+++ src/memory/memory/process_list.cpp
@@ -3,37 +3,13 @@
 
 #include <hadesmem/process_list.hpp>
 
-#include <hadesmem/detail/warning_disable_prefix.hpp>
-#include <boost/optional.hpp>
-#include <hadesmem/detail/warning_disable_suffix.hpp>
-
 #include <windows.h>
 #include <tlhelp32.h>
 
 #include <hadesmem/error.hpp>
-#include <hadesmem/config.hpp>
-#include <hadesmem/process.hpp>
-#include <hadesmem/detail/assert.hpp>
-#include <hadesmem/process_entry.hpp>
-#include <hadesmem/detail/smart_handle.hpp>
 
 namespace hadesmem
 {
-
-struct ProcessIterator::Impl
-{
-  explicit Impl() HADESMEM_NOEXCEPT
-    : snap_(INVALID_HANDLE_VALUE, INVALID_HANDLE_VALUE), 
-    process_()
-  { }
-  
-  detail::SmartHandle snap_;
-  boost::optional<ProcessEntry> process_;
-};
-
-ProcessIterator::ProcessIterator() HADESMEM_NOEXCEPT
-  : impl_()
-{ }
 
 // TOOD: Clean this up.
 ProcessIterator::ProcessIterator(int /*dummy*/)
@@ -87,46 +63,6 @@ ProcessIterator::ProcessIterator(int /*dummy*/)
   impl_->process_ = ProcessEntry(entry);
 }
 
-ProcessIterator::ProcessIterator(ProcessIterator const& other) 
-  HADESMEM_NOEXCEPT
-  : impl_(other.impl_)
-{ }
-
-ProcessIterator& ProcessIterator::operator=(ProcessIterator const& other) 
-  HADESMEM_NOEXCEPT
-{
-  impl_ = other.impl_;
-
-  return *this;
-}
-
-ProcessIterator::ProcessIterator(ProcessIterator&& other) HADESMEM_NOEXCEPT
-  : impl_(std::move(other.impl_))
-{ }
-
-ProcessIterator& ProcessIterator::operator=(ProcessIterator&& other) 
-  HADESMEM_NOEXCEPT
-{
-  impl_ = std::move(other.impl_);
-
-  return *this;
-}
-
-ProcessIterator::~ProcessIterator()
-{ }
-
-ProcessIterator::reference ProcessIterator::operator*() const HADESMEM_NOEXCEPT
-{
-  HADESMEM_ASSERT(impl_.get());
-  return *impl_->process_;
-}
-
-ProcessIterator::pointer ProcessIterator::operator->() const HADESMEM_NOEXCEPT
-{
-  HADESMEM_ASSERT(impl_.get());
-  return &*impl_->process_;
-}
-
 ProcessIterator& ProcessIterator::operator++()
 {
   HADESMEM_ASSERT(impl_.get());
@@ -153,48 +89,6 @@ ProcessIterator& ProcessIterator::operator++()
   impl_->process_ = ProcessEntry(entry);
   
   return *this;
-}
-
-ProcessIterator ProcessIterator::operator++(int)
-{
-  ProcessIterator iter(*this);
-  ++*this;
-  return iter;
-}
-
-bool ProcessIterator::operator==(ProcessIterator const& other) const 
-  HADESMEM_NOEXCEPT
-{
-  return impl_ == other.impl_;
-}
-
-bool ProcessIterator::operator!=(ProcessIterator const& other) const 
-  HADESMEM_NOEXCEPT
-{
-  return !(*this == other);
-}
-
-ProcessList::ProcessList() HADESMEM_NOEXCEPT
-{ }
-
-ProcessList::iterator ProcessList::begin()
-{
-  return ProcessList::iterator(0);
-}
-
-ProcessList::const_iterator ProcessList::begin() const
-{
-  return ProcessList::iterator(0);
-}
-
-ProcessList::iterator ProcessList::end() HADESMEM_NOEXCEPT
-{
-  return ProcessList::iterator();
-}
-
-ProcessList::const_iterator ProcessList::end() const HADESMEM_NOEXCEPT
-{
-  return ProcessList::iterator();
 }
 
 }
