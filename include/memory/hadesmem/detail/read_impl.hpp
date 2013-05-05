9457cf256fd9a4cd6a606cb434a3c1ba05726903
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 12:45:57 +0000

    * Inline Read impl.

--- include/memory/hadesmem/detail/read_impl.hpp
+++ include/memory/hadesmem/detail/read_impl.hpp
@@ -7,19 +7,48 @@
 
 #include <windows.h>
 
+#include <hadesmem/error.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/protect.hpp>
+#include <hadesmem/detail/assert.hpp>
+#include <hadesmem/detail/query_region.hpp>
+#include <hadesmem/detail/protect_guard.hpp>
+
 namespace hadesmem
 {
-
-class Process;
 
 namespace detail
 {
 
-void Read(Process const& process, LPVOID address, LPVOID data, 
-  std::size_t len);
+inline void ReadUnchecked(Process const& process, LPVOID address, 
+  LPVOID data, std::size_t len)
+{
+  HADESMEM_ASSERT(data != nullptr);
+  HADESMEM_ASSERT(len != 0);
+  
+  SIZE_T bytes_read = 0;
+  if (!::ReadProcessMemory(process.GetHandle(), address, data, len, 
+    &bytes_read) || bytes_read != len)
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("Could not read process memory.") << 
+      ErrorCodeWinLast(last_error));
+  }
+}
 
-void ReadUnchecked(Process const& process, LPVOID address, LPVOID data, 
-  std::size_t len);
+inline void Read(Process const& process, LPVOID address, LPVOID data, 
+  std::size_t len)
+{
+  HADESMEM_ASSERT(data != nullptr);
+  HADESMEM_ASSERT(len != 0);
+
+  ProtectGuard protect_guard(process, address, ProtectGuardType::kRead);
+
+  ReadUnchecked(process, address, data, len);
+
+  protect_guard.Restore();
+}
 
 }
 
