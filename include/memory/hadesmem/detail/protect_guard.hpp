2cd9044c1a447697d106ceed28eb50cb5879acbe
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 11:51:53 +0000

    * Inline ProtectGuard.

--- include/memory/hadesmem/detail/protect_guard.hpp
+++ include/memory/hadesmem/detail/protect_guard.hpp
@@ -4,10 +4,16 @@
 #pragma once
 
 #include <memory>
+#include <utility>
 
 #include <windows.h>
 
+#include <hadesmem/error.hpp>
 #include <hadesmem/config.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/protect.hpp>
+#include <hadesmem/detail/query_region.hpp>
+#include <hadesmem/detail/protect_region.hpp>
 
 namespace hadesmem
 {
@@ -27,24 +33,95 @@ class ProtectGuard
 {
 public:
   explicit ProtectGuard(Process const& process, PVOID address, 
-    ProtectGuardType type);
+    ProtectGuardType type)
+    : process_(&process), 
+    type_(type), 
+    can_read_or_write_(false), 
+    old_protect_(0), 
+    mbi_(Query(process, address))
+  {
+    if (IsGuard(mbi_))
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Attempt to write to guard page."));
+    }
 
-  ProtectGuard(ProtectGuard&& other) HADESMEM_NOEXCEPT;
+    can_read_or_write_ = (type_ == ProtectGuardType::kRead) ? 
+      CanRead(mbi_) : CanWrite(mbi_);
 
-  ProtectGuard& operator=(ProtectGuard&& other) HADESMEM_NOEXCEPT;
+    if (!can_read_or_write_)
+    {
+      old_protect_ = Protect(process, mbi_, PAGE_EXECUTE_READWRITE);
+    }
+  }
 
-  ~ProtectGuard();
+  ProtectGuard(ProtectGuard&& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    type_(other.type_), 
+    can_read_or_write_(other.can_read_or_write_), 
+    old_protect_(other.old_protect_), 
+    mbi_(other.mbi_)
+  {
+    other.old_protect_ = 0;
+  }
 
-  void Restore();
+  ProtectGuard& operator=(ProtectGuard&& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    type_ = other.type_;
+    can_read_or_write_ = other.can_read_or_write_;
+    old_protect_ = other.old_protect_;
+    mbi_ = other.mbi_;
 
-  void RestoreUnchecked() HADESMEM_NOEXCEPT;
+    other.old_protect_ = 0;
+
+    return *this;
+  }
+
+  ~ProtectGuard() HADESMEM_NOEXCEPT
+  {
+    RestoreUnchecked();
+  }
+  
+  void Restore()
+  {
+    if (!old_protect_)
+    {
+      return;
+    }
+
+    if (!can_read_or_write_)
+    {
+      Protect(*process_, mbi_, old_protect_);
+    }
+
+    old_protect_ = 0;
+  }
+
+  void RestoreUnchecked() HADESMEM_NOEXCEPT
+  {
+    try
+    {
+      Restore();
+    }
+    catch (std::exception const& e)
+    {
+      (void)e;
+
+      // WARNING: Protection is not restored if 'Restore' fails.
+      assert(boost::diagnostic_information(e).c_str() && false);
+    }
+  }
 
 private:
   ProtectGuard(ProtectGuard const& other) HADESMEM_DELETED_FUNCTION;
   ProtectGuard& operator=(ProtectGuard const& other) HADESMEM_DELETED_FUNCTION;
-
-  struct Impl;
-  std::unique_ptr<Impl> impl_;
+  
+  Process const* process_;
+  ProtectGuardType type_;
+  bool can_read_or_write_;
+  DWORD old_protect_;
+  MEMORY_BASIC_INFORMATION mbi_;
 };
 
 }
