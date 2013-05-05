7dbdb9bdee45ab798bb18bcc71bf86c90cdac614
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 13:01:05 +0000

    * Inline write impl.

--- include/memory/hadesmem/detail/write_impl.hpp
+++ include/memory/hadesmem/detail/write_impl.hpp
@@ -7,6 +7,13 @@
 
 #include <windows.h>
 
+#include <hadesmem/error.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/protect.hpp>
+#include <hadesmem/detail/assert.hpp>
+#include <hadesmem/detail/query_region.hpp>
+#include <hadesmem/detail/protect_guard.hpp>
+
 namespace hadesmem
 {
 
@@ -14,9 +21,27 @@ class Process;
 
 namespace detail
 {
+  
+inline void Write(Process const& process, PVOID address, LPCVOID data, 
+  std::size_t len)
+{
+  HADESMEM_ASSERT(data != nullptr);
+  HADESMEM_ASSERT(len != 0);
 
-void Write(Process const& process, PVOID address, LPCVOID data, 
-  std::size_t len);
+  ProtectGuard protect_guard(process, address, ProtectGuardType::kWrite);
+  
+  SIZE_T bytes_written = 0;
+  if (!::WriteProcessMemory(process.GetHandle(), address, data, 
+    len, &bytes_written) || bytes_written != len)
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("Could not write process memory.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  protect_guard.Restore();
+}
 
 }
 
