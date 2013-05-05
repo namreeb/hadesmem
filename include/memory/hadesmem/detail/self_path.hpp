da109b5d1b29f0be080c6fa30564ee46a2138972
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 12:50:37 +0000

    * Inline self path APIs.

--- include/memory/hadesmem/detail/self_path.hpp
+++ include/memory/hadesmem/detail/self_path.hpp
@@ -4,8 +4,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include <windows.h>
+
+#include <hadesmem/error.hpp>
+#include <hadesmem/config.hpp>
+#include <hadesmem/detail/assert.hpp>
 
 namespace hadesmem
 {
@@ -13,11 +18,47 @@ namespace hadesmem
 namespace detail
 {
 
-HMODULE GetHandleToSelf();
+inline HMODULE GetHandleToSelf()
+{
+  MEMORY_BASIC_INFORMATION mem_info;
+  ZeroMemory(&mem_info, sizeof(mem_info));
+  auto const this_func_ptr = reinterpret_cast<LPCVOID>(
+    reinterpret_cast<DWORD_PTR>(&GetHandleToSelf));
+  if (!::VirtualQuery(this_func_ptr, &mem_info, sizeof(mem_info)))
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("Failed to query memory.") << 
+      ErrorCodeWinLast(last_error));
+  }
 
-std::wstring GetSelfPath();
+  return static_cast<HMODULE>(mem_info.AllocationBase);
+}
 
-std::wstring GetSelfDirPath();
+inline std::wstring GetSelfPath()
+{
+  std::vector<wchar_t> path(HADESMEM_MAX_PATH_UNICODE);
+  DWORD const path_out_len = ::GetModuleFileName(GetHandleToSelf(), 
+    path.data(), static_cast<DWORD>(path.size()));
+  if (!path_out_len || ::GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("GetModuleFileName failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  return path.data();
+}
+
+inline std::wstring GetSelfDirPath()
+{
+  std::wstring self_path(GetSelfPath());
+  std::wstring::size_type const separator = self_path.rfind(L'\\');
+  HADESMEM_ASSERT(separator != std::wstring::npos);
+  self_path.erase(separator);
+  return self_path;
+}
 
 }
 
