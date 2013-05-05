15ec932512bd18435bda8ea6f80cc34c9760dc18
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 12:53:35 +0000

    * Inline ToUpperOrdinal.

--- include/memory/hadesmem/detail/to_upper_ordinal.hpp
+++ include/memory/hadesmem/detail/to_upper_ordinal.hpp
@@ -3,7 +3,14 @@
 
 #pragma once
 
+#include <limits>
 #include <string>
+#include <vector>
+#include <cassert>
+
+#include <windows.h>
+
+#include <hadesmem/error.hpp>
 
 namespace hadesmem
 {
@@ -11,7 +18,25 @@ namespace hadesmem
 namespace detail
 {
 
-std::wstring ToUpperOrdinal(std::wstring const& str);
+inline std::wstring ToUpperOrdinal(std::wstring const& str)
+{
+  std::vector<wchar_t> str_buf(std::begin(str), std::end(str));
+  str_buf.push_back(0);
+  
+  assert(str_buf.size() < (std::numeric_limits<DWORD>::max)());
+  DWORD const num_converted = ::CharUpperBuff(str_buf.data(), 
+    static_cast<DWORD>(str_buf.size()));
+  if (num_converted != str_buf.size())
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("CharUpperBuff failed.") << 
+      ErrorCodeWinRet(num_converted) << 
+      ErrorCodeWinLast(last_error));
+  }
+  
+  return str_buf.data();
+}
 
 }
 
