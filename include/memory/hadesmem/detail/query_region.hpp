14817234670fb9d56d176d1422f6a456ce73b0e8
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 12:13:38 +0000

    * Inline region query APIs.

--- include/memory/hadesmem/detail/query_region.hpp
+++ include/memory/hadesmem/detail/query_region.hpp
@@ -5,7 +5,9 @@
 
 #include <windows.h>
 
+#include <hadesmem/error.hpp>
 #include <hadesmem/config.hpp>
+#include <hadesmem/process.hpp>
 
 namespace hadesmem
 {
@@ -15,15 +17,56 @@ class Process;
 namespace detail
 {
 
-MEMORY_BASIC_INFORMATION Query(Process const& process, LPCVOID address);
+inline MEMORY_BASIC_INFORMATION Query(Process const& process, LPCVOID address)
+{
+  MEMORY_BASIC_INFORMATION mbi;
+  ::ZeroMemory(&mbi, sizeof(mbi));
+  if (::VirtualQueryEx(process.GetHandle(), address, &mbi, sizeof(mbi)) != 
+    sizeof(mbi))
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("VirtualQueryEx failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+  
+  return mbi;
+}
 
-bool CanRead(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT;
+inline bool CanRead(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT
+{
+  return (mbi.State != MEM_RESERVE) && 
+    ((mbi.Protect & PAGE_EXECUTE_READ) == PAGE_EXECUTE_READ || 
+    (mbi.Protect & PAGE_EXECUTE_READWRITE) == PAGE_EXECUTE_READWRITE || 
+    (mbi.Protect & PAGE_EXECUTE_WRITECOPY) == PAGE_EXECUTE_WRITECOPY || 
+    (mbi.Protect & PAGE_READONLY) == PAGE_READONLY || 
+    (mbi.Protect & PAGE_READWRITE) == PAGE_READWRITE || 
+    (mbi.Protect & PAGE_WRITECOPY) == PAGE_WRITECOPY);
+}
 
-bool CanWrite(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT;
+inline bool CanWrite(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT
+{
+  return (mbi.State != MEM_RESERVE) && 
+    ((mbi.Protect & PAGE_EXECUTE_READWRITE) == PAGE_EXECUTE_READWRITE || 
+    (mbi.Protect & PAGE_EXECUTE_WRITECOPY) == PAGE_EXECUTE_WRITECOPY || 
+    (mbi.Protect & PAGE_READWRITE) == PAGE_READWRITE || 
+    (mbi.Protect & PAGE_WRITECOPY) == PAGE_WRITECOPY);
+}
 
-bool CanExecute(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT;
+inline bool CanExecute(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT
+{
+  return (mbi.State != MEM_RESERVE) && 
+    ((mbi.Protect & PAGE_EXECUTE) == PAGE_EXECUTE || 
+    (mbi.Protect & PAGE_EXECUTE_READ) == PAGE_EXECUTE_READ || 
+    (mbi.Protect & PAGE_EXECUTE_READWRITE) == PAGE_EXECUTE_READWRITE || 
+    (mbi.Protect & PAGE_EXECUTE_WRITECOPY) == PAGE_EXECUTE_WRITECOPY);
+}
 
-bool IsGuard(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT;
+inline bool IsGuard(MEMORY_BASIC_INFORMATION const& mbi) HADESMEM_NOEXCEPT
+{
+  return (mbi.Protect & PAGE_GUARD) == PAGE_GUARD;
+}
+
 }
 
 }
