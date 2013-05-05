3dc7e84d4924a71f2a30ea32166bc42858ffb730
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-05 10:59:27 +0000

    * Inline Protect.

--- include/memory/hadesmem/protect.hpp
+++ include/memory/hadesmem/protect.hpp
@@ -5,19 +5,44 @@
 
 #include <windows.h>
 
+#include <hadesmem/error.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/detail/query_region.hpp>
+#include <hadesmem/detail/protect_region.hpp>
+
 namespace hadesmem
 {
 
 class Process;
 
-bool CanRead(Process const& process, LPCVOID address);
+inline bool CanRead(Process const& process, LPCVOID address)
+{
+  MEMORY_BASIC_INFORMATION const mbi = detail::Query(process, address);
+  return detail::CanRead(mbi);
+}
 
-bool CanWrite(Process const& process, LPCVOID address);
+inline bool CanWrite(Process const& process, LPCVOID address)
+{
+  MEMORY_BASIC_INFORMATION const mbi = detail::Query(process, address);
+  return detail::CanWrite(mbi);
+}
 
-bool CanExecute(Process const& process, LPCVOID address);
+inline bool CanExecute(Process const& process, LPCVOID address)
+{
+  MEMORY_BASIC_INFORMATION const mbi = detail::Query(process, address);
+  return detail::CanExecute(mbi);
+}
 
-bool IsGuard(Process const& process, LPCVOID address);
+inline bool IsGuard(Process const& process, LPCVOID address)
+{
+  MEMORY_BASIC_INFORMATION const mbi = detail::Query(process, address);
+  return detail::IsGuard(mbi);
+}
 
-DWORD Protect(Process const& process, LPVOID address, DWORD protect);
+inline DWORD Protect(Process const& process, LPVOID address, DWORD protect)
+{
+  MEMORY_BASIC_INFORMATION const mbi = detail::Query(process, address);
+  return detail::Protect(process, mbi, protect);
+}
 
 }
