f3d386f2b9559f84b4e4112eb162e6e60ccd6244
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-11 05:59:44 +0000

    * Move detail functions to detail header.

---
+++ include/memory/hadesmem/detail/winapi.hpp
@@ -0,0 +1,64 @@
+// Copyright (C) 2010-2013 Joshua Boyce.
+// See the file COPYING for copying permission.
+
+#pragma once
+
+#include <windows.h>
+
+#include <hadesmem/error.hpp>
+#include <hadesmem/detail/assert.hpp>
+
+namespace hadesmem
+{
+
+namespace detail
+{
+
+inline bool IsWoW64Process(HANDLE handle)
+{
+  BOOL is_wow64 = FALSE;
+  if (!::IsWow64Process(handle, &is_wow64))
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("IsWoW64Process failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  return is_wow64 != FALSE;
+}
+
+inline HANDLE OpenProcessAllAccess(DWORD id)
+{
+  HANDLE const handle = ::OpenProcess(PROCESS_ALL_ACCESS, TRUE, id);
+  if (!handle)
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("OpenProcess failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  return handle;
+}
+
+inline HANDLE DuplicateHandle(HANDLE handle)
+{
+  HADESMEM_ASSERT(handle != nullptr);
+
+  HANDLE new_handle = nullptr;
+  if (!::DuplicateHandle(::GetCurrentProcess(), handle, 
+    ::GetCurrentProcess(), &new_handle, 0, TRUE, DUPLICATE_SAME_ACCESS))
+  {
+    DWORD const last_error = ::GetLastError();
+    HADESMEM_THROW_EXCEPTION(Error() << 
+      ErrorString("DuplicateHandle failed.") << 
+      ErrorCodeWinLast(last_error));
+  }
+
+  return new_handle;
+}
+
+}
+
+}
--- include/memory/hadesmem/process.hpp
+++ include/memory/hadesmem/process.hpp
@@ -14,60 +14,11 @@
 #include <hadesmem/error.hpp>
 #include <hadesmem/config.hpp>
 #include <hadesmem/detail/assert.hpp>
+#include <hadesmem/detail/winapi.hpp>
 #include <hadesmem/detail/smart_handle.hpp>
 
 namespace hadesmem
 {
-
-namespace detail
-{
-  
-inline bool IsWoW64Process(HANDLE handle)
-{
-  BOOL is_wow64 = FALSE;
-  if (!::IsWow64Process(handle, &is_wow64))
-  {
-    DWORD const last_error = ::GetLastError();
-    HADESMEM_THROW_EXCEPTION(Error() << 
-      ErrorString("IsWoW64Process failed.") << 
-      ErrorCodeWinLast(last_error));
-  }
-
-  return is_wow64 != FALSE;
-}
-
-inline HANDLE OpenProcessAllAccess(DWORD id)
-{
-  HANDLE const handle = ::OpenProcess(PROCESS_ALL_ACCESS, TRUE, id);
-  if (!handle)
-  {
-    DWORD const last_error = ::GetLastError();
-    HADESMEM_THROW_EXCEPTION(Error() << 
-      ErrorString("OpenProcess failed.") << 
-      ErrorCodeWinLast(last_error));
-  }
-
-  return handle;
-}
-
-inline HANDLE DuplicateHandle(HANDLE handle)
-{
-  HADESMEM_ASSERT(handle != nullptr);
-
-  HANDLE new_handle = nullptr;
-  if (!::DuplicateHandle(::GetCurrentProcess(), handle, 
-    ::GetCurrentProcess(), &new_handle, 0, TRUE, DUPLICATE_SAME_ACCESS))
-  {
-    DWORD const last_error = ::GetLastError();
-    HADESMEM_THROW_EXCEPTION(Error() << 
-      ErrorString("DuplicateHandle failed.") << 
-      ErrorCodeWinLast(last_error));
-  }
-
-  return new_handle;
-}
-
-}
 
 class Process
 {
