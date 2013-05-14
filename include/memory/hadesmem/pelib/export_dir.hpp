5df0b6b0603c56c305ee1bd1427c3b41d73c18c3
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-14 14:15:56 +0000

    * Remove PImpl from ExportDir. * Inline ExportDir.

--- include/memory/hadesmem/pelib/export_dir.hpp
+++ include/memory/hadesmem/pelib/export_dir.hpp
@@ -6,98 +6,302 @@
 #include <iosfwd>
 #include <memory>
 #include <string>
+#include <cstddef>
+#include <ostream>
+#include <utility>
+
+#include <hadesmem/detail/warning_disable_prefix.hpp>
+#include <boost/assert.hpp>
+#include <hadesmem/detail/warning_disable_suffix.hpp>
 
 #include <windows.h>
+#include <winnt.h>
 
+#include <hadesmem/read.hpp>
+#include <hadesmem/error.hpp>
+#include <hadesmem/write.hpp>
 #include <hadesmem/config.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/pelib/pe_file.hpp>
+#include <hadesmem/pelib/nt_headers.hpp>
+
+// TODO: Fix the code so this hack can be removed.
+#if defined(HADESMEM_CLANG)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextended-offsetof"
+#endif
 
 namespace hadesmem
 {
 
-class Process;
-
-class PeFile;
-
 class ExportDir
 {
 public:
-  explicit ExportDir(Process const& process, PeFile const& pe_file);
+  explicit ExportDir(Process const& process, PeFile const& pe_file)
+    : process_(&process), 
+    pe_file_(&pe_file), 
+    base_(nullptr)
+  {
+    NtHeaders nt_headers(process, pe_file);
+    DWORD const export_dir_rva = nt_headers.GetDataDirectoryVirtualAddress(
+      PeDataDir::Export);
+    // Windows will load images which don't specify a size for the export 
+    // directory.
+    if (!export_dir_rva)
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Export directory is invalid."));
+    }
 
-  ExportDir(ExportDir const& other);
+    base_ = static_cast<PBYTE>(RvaToVa(process, pe_file, export_dir_rva));
+  }
+
+  ExportDir(ExportDir const& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    pe_file_(other.pe_file_), 
+    base_(other.base_)
+  { }
   
-  ExportDir& operator=(ExportDir const& other);
+  ExportDir& operator=(ExportDir const& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
+    base_ = other.base_;
 
-  ExportDir(ExportDir&& other) HADESMEM_NOEXCEPT;
+    return *this;
+  }
+
+  ExportDir(ExportDir&& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    pe_file_(other.pe_file_), 
+    base_(other.base_)
+  { }
   
-  ExportDir& operator=(ExportDir&& other) HADESMEM_NOEXCEPT;
+  ExportDir& operator=(ExportDir&& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
+    base_ = other.base_;
+
+    return *this;
+  }
   
-  ~ExportDir();
+  ~ExportDir() HADESMEM_NOEXCEPT
+  { }
 
-  PVOID GetBase() const HADESMEM_NOEXCEPT;
-
-  DWORD GetCharacteristics() const;
-
-  DWORD GetTimeDateStamp() const;
-
-  WORD GetMajorVersion() const;
-
-  WORD GetMinorVersion() const;
-
-  std::string GetName() const;
-
-  DWORD GetOrdinalBase() const;
-
-  DWORD GetNumberOfFunctions() const;
-
-  DWORD GetNumberOfNames() const;
-
-  DWORD GetAddressOfFunctions() const;
-
-  DWORD GetAddressOfNames() const;
-
-  DWORD GetAddressOfNameOrdinals() const;
-
-  void SetCharacteristics(DWORD characteristics);
-
-  void SetTimeDateStamp(DWORD time_date_stamp);
-
-  void SetMajorVersion(WORD major_version);
-
-  void SetMinorVersion(WORD minor_version);
-
-  void SetName(std::string const& name);
-
-  void SetOrdinalBase(DWORD ordinal_base);
-
-  void SetNumberOfFunctions(DWORD number_of_functions);
-
-  void SetNumberOfNames(DWORD number_of_names);
-
-  void SetAddressOfFunctions(DWORD address_of_functions);
-
-  void SetAddressOfNames(DWORD address_of_names);
-
-  void SetAddressOfNameOrdinals(DWORD address_of_name_ordinals);
+  PVOID GetBase() const HADESMEM_NOEXCEPT
+  {
+    return base_;
+  }
   
+  DWORD GetCharacteristics() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, Characteristics));
+  }
+
+  DWORD GetTimeDateStamp() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, TimeDateStamp));
+  }
+
+  WORD GetMajorVersion() const
+  {
+    return Read<WORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, MajorVersion));
+  }
+
+  WORD GetMinorVersion() const
+  {
+    return Read<WORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, MinorVersion));
+  }
+
+  std::string GetName() const
+  {
+    DWORD const name_rva = Read<DWORD>(*process_, base_ + 
+      FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, Name));
+
+    if (!name_rva)
+    {
+      return std::string();
+    }
+
+    PVOID name_va = RvaToVa(*process_, *pe_file_, name_rva);
+    return ReadString<char>(*process_, name_va);
+  }
+
+  DWORD GetOrdinalBase() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, Base));
+  }
+
+  DWORD GetNumberOfFunctions() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, NumberOfFunctions));
+  }
+
+  DWORD GetNumberOfNames() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, NumberOfNames));
+  }
+
+  DWORD GetAddressOfFunctions() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, AddressOfFunctions));
+  }
+
+  DWORD GetAddressOfNames() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, AddressOfNames));
+  }
+
+  DWORD GetAddressOfNameOrdinals() const
+  {
+    return Read<DWORD>(*process_, base_ + FIELD_OFFSET(
+      IMAGE_EXPORT_DIRECTORY, AddressOfNameOrdinals));
+  }
+
+  void SetCharacteristics(DWORD characteristics)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      Characteristics), characteristics);
+  }
+
+  void SetTimeDateStamp(DWORD time_date_stamp)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      TimeDateStamp), time_date_stamp);
+  }
+
+  void SetMajorVersion(WORD major_version)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      MajorVersion), major_version);
+  }
+
+  void SetMinorVersion(WORD minor_version)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      MinorVersion), minor_version);
+  }
+
+  void SetName(std::string const& name)
+  {
+    DWORD const name_rva = Read<DWORD>(*process_, base_ + 
+      FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, Name));
+
+    if (!name_rva)
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Export dir has no name. Cannot overwrite."));
+    }
+
+    std::string const current_name = ReadString<char>(*process_, 
+      RvaToVa(*process_, *pe_file_, name_rva));
+
+    // TODO: Support allocating space for a new name rather than just 
+    // overwriting the existing one.
+    if (name.size() > current_name.size())
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Cannot overwrite name with longer string."));
+    }
+
+    WriteString(*process_, RvaToVa(*process_, *pe_file_, 
+      name_rva), name);
+  }
+
+  void SetOrdinalBase(DWORD base)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      Base), base);
+  }
+
+  void SetNumberOfFunctions(DWORD number_of_functions)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      NumberOfFunctions), number_of_functions);
+  }
+
+  void SetNumberOfNames(DWORD number_of_names)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      NumberOfNames), number_of_names);
+  }
+
+  void SetAddressOfFunctions(DWORD address_of_functions)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      AddressOfFunctions), address_of_functions);
+  }
+
+  void SetAddressOfNames(DWORD address_of_names)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      AddressOfNames), address_of_names);
+  }
+
+  void SetAddressOfNameOrdinals(DWORD address_of_name_ordinals)
+  {
+    Write(*process_, base_ + FIELD_OFFSET(IMAGE_EXPORT_DIRECTORY, 
+      AddressOfNameOrdinals), address_of_name_ordinals);
+  }
+
 private:
-  struct Impl;
-  std::unique_ptr<Impl> impl_;
+  Process const* process_;
+  PeFile const* pe_file_;
+  PBYTE base_;
 };
 
-bool operator==(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator==(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetBase() == rhs.GetBase();
+}
 
-bool operator!=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator!=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return !(lhs == rhs);
+}
 
-bool operator<(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator<(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetBase() < rhs.GetBase();
+}
 
-bool operator<=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator<=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetBase() <= rhs.GetBase();
+}
 
-bool operator>(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator>(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetBase() > rhs.GetBase();
+}
 
-bool operator>=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator>=(ExportDir const& lhs, ExportDir const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetBase() >= rhs.GetBase();
+}
 
-std::ostream& operator<<(std::ostream& lhs, ExportDir const& rhs);
+inline std::ostream& operator<<(std::ostream& lhs, ExportDir const& rhs)
+{
+  return (lhs << rhs.GetBase());
+}
 
-std::wostream& operator<<(std::wostream& lhs, ExportDir const& rhs);
+inline std::wostream& operator<<(std::wostream& lhs, ExportDir const& rhs)
+{
+  return (lhs << rhs.GetBase());
+}
 
 }
+
+#if defined(HADESMEM_CLANG)
+#pragma GCC diagnostic pop
+#endif
