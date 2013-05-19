3cdcd01848ad25c66376a74bc67b50d6e385d3aa
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-19 10:34:37 +0000

    * Inline Export. * Remove PImpl from Export.

--- include/memory/hadesmem/pelib/export.hpp
+++ include/memory/hadesmem/pelib/export.hpp
@@ -6,17 +6,35 @@
 #include <iosfwd>
 #include <memory>
 #include <string>
+#include <cstddef>
+#include <ostream>
+#include <utility>
+
+#include <hadesmem/detail/warning_disable_prefix.hpp>
+#include <boost/assert.hpp>
+#include <boost/lexical_cast.hpp>
+#include <hadesmem/detail/warning_disable_suffix.hpp>
 
 #include <windows.h>
+#include <winnt.h>
 
+#include <hadesmem/read.hpp>
+#include <hadesmem/error.hpp>
+#include <hadesmem/write.hpp>
 #include <hadesmem/config.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/pelib/pe_file.hpp>
+#include <hadesmem/pelib/export_dir.hpp>
+#include <hadesmem/pelib/nt_headers.hpp>
+
+// TODO: Fix the code so this hack can be removed.
+#if defined(HADESMEM_CLANG)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextended-offsetof"
+#endif
 
 namespace hadesmem
 {
-
-class Process;
-
-class PeFile;
 
 // TODO: Constructor to create Export by name (optimize using binary search).
 
@@ -24,61 +42,281 @@ class Export
 {
 public:
   explicit Export(Process const& process, PeFile const& pe_file, 
-    WORD ordinal);
+    WORD ordinal)
+    : process_(&process), 
+    pe_file_(&pe_file), 
+    rva_(0), 
+    va_(nullptr), 
+    name_(), 
+    forwarder_(), 
+    forwarder_split_(), 
+    ordinal_(ordinal), 
+    by_name_(false), 
+    forwarded_(false)
+  {
+    ExportDir const export_dir(process, pe_file);
+
+    WORD offset = static_cast<WORD>(ordinal_ - export_dir.GetOrdinalBase());
+    
+    if (offset >= export_dir.GetNumberOfFunctions())
+    {
+        HADESMEM_THROW_EXCEPTION(Error() << 
+          ErrorString("Ordinal out of range."));
+    }
+
+    WORD* ptr_ordinals = static_cast<WORD*>(RvaToVa(process, pe_file, 
+      export_dir.GetAddressOfNameOrdinals()));
+
+    DWORD* ptr_names = static_cast<DWORD*>(RvaToVa(process, pe_file, 
+      export_dir.GetAddressOfNames()));
+
+    if (DWORD const num_names = export_dir.GetNumberOfNames())
+    {
+      std::vector<WORD> name_ordinals = ReadVector<WORD>(process, 
+        ptr_ordinals, num_names);
+      auto name_ord_iter = std::find(std::begin(name_ordinals), 
+        std::end(name_ordinals), offset);
+      if (name_ord_iter != std::end(name_ordinals))
+      {
+        by_name_ = true;
+        DWORD const name_rva = Read<DWORD>(process, ptr_names + std::distance(
+          std::begin(name_ordinals), name_ord_iter));
+        name_ = ReadString<char>(process, RvaToVa(process, pe_file, name_rva));
+      }
+    }
+
+    DWORD* ptr_functions = static_cast<DWORD*>(RvaToVa(process, pe_file, 
+      export_dir.GetAddressOfFunctions()));
+    DWORD const func_rva = Read<DWORD>(process, ptr_functions + offset);
+
+    NtHeaders const nt_headers(process, pe_file);
+
+    DWORD const export_dir_start = nt_headers.GetDataDirectoryVirtualAddress(
+      PeDataDir::Export);
+    DWORD const export_dir_end = export_dir_start + 
+      nt_headers.GetDataDirectorySize(PeDataDir::Export);
+
+    // Check function RVA. If it lies inside the export dir region 
+    // then it's a forwarded export. Otherwise it's a regular RVA.
+    if (func_rva > export_dir_start && func_rva < export_dir_end)
+    {
+      forwarded_ = true;
+      forwarder_ = ReadString<char>(process, RvaToVa(process, pe_file, 
+        func_rva));
+
+      std::string::size_type split_pos = forwarder_.rfind('.');
+      if (split_pos != std::string::npos)
+      {
+        forwarder_split_ = std::make_pair(forwarder_.substr(0, split_pos), 
+          forwarder_.substr(split_pos + 1));
+      }
+      else
+      {
+        HADESMEM_THROW_EXCEPTION(Error() << 
+          ErrorString("Invalid forwarder string format."));
+      }
+    }
+    else
+    {
+      rva_ = func_rva;
+      va_ = RvaToVa(process, pe_file, func_rva);
+    }
+  }
   
-  Export(Export const& other);
+  Export(Export const& other)
+    : process_(other.process_), 
+    pe_file_(other.pe_file_), 
+    rva_(other.rva_), 
+    va_(other.va_), 
+    name_(other.name_), 
+    forwarder_(other.forwarder_), 
+    forwarder_split_(other.forwarder_split_), 
+    ordinal_(other.ordinal_), 
+    by_name_(other.by_name_), 
+    forwarded_(other.forwarded_)
+  { }
   
-  Export& operator=(Export const& other);
+  Export& operator=(Export const& other)
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
+    rva_ = other.rva_;
+    va_ = other.va_;
+    name_ = other.name_;
+    forwarder_ = other.forwarder_;
+    forwarder_split_ = other.forwarder_split_;
+    ordinal_ = other.ordinal_;
+    by_name_ = other.by_name_;
+    forwarded_ = other.forwarded_;
 
-  Export(Export&& other) HADESMEM_NOEXCEPT;
+    return *this;
+  }
+
+  Export(Export&& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    pe_file_(other.pe_file_), 
+    rva_(other.rva_), 
+    va_(other.va_), 
+    name_(std::move(other.name_)), 
+    forwarder_(std::move(other.forwarder_)), 
+    forwarder_split_(std::move(other.forwarder_split_)), 
+    ordinal_(other.ordinal_), 
+    by_name_(other.by_name_), 
+    forwarded_(other.forwarded_)
+  { }
   
-  Export& operator=(Export&& other) HADESMEM_NOEXCEPT;
+  Export& operator=(Export&& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
+    rva_ = other.rva_;
+    va_ = other.va_;
+    name_ = std::move(other.name_);
+    forwarder_ = std::move(other.forwarder_);
+    forwarder_split_ = std::move(other.forwarder_split_);
+    ordinal_ = other.ordinal_;
+    by_name_ = other.by_name_;
+    forwarded_ = other.forwarded_;
+
+    return *this;
+  }
   
-  ~Export();
+  ~Export() HADESMEM_NOEXCEPT
+  { }
 
-  DWORD GetRva() const;
+  DWORD GetRva() const HADESMEM_NOEXCEPT
+  {
+    return rva_;
+  }
 
-  PVOID GetVa() const;
+  PVOID GetVa() const HADESMEM_NOEXCEPT
+  {
+    return va_;
+  }
 
-  std::string GetName() const;
+  std::string GetName() const
+  {
+    return name_;
+  }
 
-  WORD GetOrdinal() const;
+  WORD GetOrdinal() const HADESMEM_NOEXCEPT
+  {
+    return ordinal_;
+  }
 
-  bool ByName() const;
+  bool ByName() const HADESMEM_NOEXCEPT
+  {
+    return by_name_;
+  }
 
-  bool ByOrdinal() const;
+  bool ByOrdinal() const HADESMEM_NOEXCEPT
+  {
+    return !by_name_;
+  }
 
-  bool IsForwarded() const;
+  bool IsForwarded() const HADESMEM_NOEXCEPT
+  {
+    return forwarded_;
+  }
 
-  std::string GetForwarder() const;
+  std::string GetForwarder() const
+  {
+    return forwarder_;
+  }
 
-  std::string GetForwarderModule() const;
+  std::string GetForwarderModule() const
+  {
+    return forwarder_split_.first;
+  }
 
-  std::string GetForwarderFunction() const;
+  std::string GetForwarderFunction() const
+  {
+    return forwarder_split_.second;
+  }
 
-  bool IsForwardedByOrdinal() const;
+  bool IsForwardedByOrdinal() const
+  {
+    return (GetForwarderFunction()[0] == '#');
+  }
 
-  WORD GetForwarderOrdinal() const;
+  WORD GetForwarderOrdinal() const
+  {
+    if (!IsForwardedByOrdinal())
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Function is not exported by ordinal."));
+    }
+
+    try
+    {
+      std::string const forwarder_function(GetForwarderFunction());
+      return boost::lexical_cast<WORD>(forwarder_function.substr(1));
+    }
+    catch (std::exception const& /*e*/)
+    {
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Invalid forwarder ordinal detected."));
+    }
+  }
 
 private:
-  struct Impl;
-  std::unique_ptr<Impl> impl_;
+  Process const* process_;
+  PeFile const* pe_file_;
+  DWORD rva_;
+  PVOID va_;
+  std::string name_;
+  std::string forwarder_;
+  std::pair<std::string, std::string> forwarder_split_;
+  WORD ordinal_;
+  bool by_name_;
+  bool forwarded_;
+  bool forwarded_by_ordinal_;
+  WORD forwarder_ordinal_;
 };
 
-bool operator==(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator==(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetOrdinal() == rhs.GetOrdinal();
+}
 
-bool operator!=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator!=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return !(lhs == rhs);
+}
 
-bool operator<(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator<(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetOrdinal() < rhs.GetOrdinal();
+}
 
-bool operator<=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator<=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetOrdinal() <= rhs.GetOrdinal();
+}
 
-bool operator>(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator>(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetOrdinal() > rhs.GetOrdinal();
+}
 
-bool operator>=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT;
+inline bool operator>=(Export const& lhs, Export const& rhs) HADESMEM_NOEXCEPT
+{
+  return lhs.GetOrdinal() >= rhs.GetOrdinal();
+}
 
-std::ostream& operator<<(std::ostream& lhs, Export const& rhs);
+inline std::ostream& operator<<(std::ostream& lhs, Export const& rhs)
+{
+  return (lhs << rhs.GetOrdinal());
+}
 
-std::wostream& operator<<(std::wostream& lhs, Export const& rhs);
+inline std::wostream& operator<<(std::wostream& lhs, Export const& rhs)
+{
+  return (lhs << rhs.GetOrdinal());
+}
 
 }
+
+// TODO: Fix the code so this hack can be removed.
+#if defined(HADESMEM_CLANG)
+#pragma GCC diagnostic pop
+#endif
