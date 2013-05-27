d7404d13f734c744891be6246149991f78fb9ee6
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-27 10:58:43 +0000

    * Inline SectionIterator and SectionList. * Remove PImpl from SectionList.

--- include/memory/hadesmem/pelib/section_list.hpp
+++ include/memory/hadesmem/pelib/section_list.hpp
@@ -4,54 +4,145 @@
 #pragma once
 
 #include <memory>
+#include <cstddef>
+#include <utility>
 #include <iterator>
 
+#include <hadesmem/detail/warning_disable_prefix.hpp>
+#include <boost/optional.hpp>
+#include <hadesmem/detail/warning_disable_suffix.hpp>
+
+#include <windows.h>
+#include <winnt.h>
+
+#include <hadesmem/error.hpp>
 #include <hadesmem/config.hpp>
+#include <hadesmem/process.hpp>
+#include <hadesmem/detail/assert.hpp>
+#include <hadesmem/pelib/section.hpp>
+#include <hadesmem/pelib/nt_headers.hpp>
 
 namespace hadesmem
 {
-
-class Process;
-
-class PeFile;
-
-class Section;
 
 // SectionIterator satisfies the requirements of an input iterator 
 // (C++ Standard, 24.2.1, Input Iterators [input.iterators]).
 class SectionIterator : public std::iterator<std::input_iterator_tag, Section>
 {
 public:
-  SectionIterator() HADESMEM_NOEXCEPT;
+  SectionIterator() HADESMEM_NOEXCEPT
+    : impl_()
+  { }
   
-  explicit SectionIterator(Process const& process, PeFile const& pe_file);
+  explicit SectionIterator(Process const& process, PeFile const& pe_file)
+    : impl_(new Impl(process, pe_file))
+  {
+    NtHeaders const nt_headers(process, pe_file);
+    if (!nt_headers.GetNumberOfSections())
+    {
+      impl_.reset();
+      return;
+    }
 
-  SectionIterator(SectionIterator const& other) HADESMEM_NOEXCEPT;
+    PBYTE base = static_cast<PBYTE>(nt_headers.GetBase()) + 
+      offsetof(IMAGE_NT_HEADERS, OptionalHeader) + 
+      nt_headers.GetSizeOfOptionalHeader();
 
-  SectionIterator& operator=(SectionIterator const& other) HADESMEM_NOEXCEPT;
+    impl_->section_ = Section(process, pe_file, 0, base);
+  }
 
-  SectionIterator(SectionIterator&& other) HADESMEM_NOEXCEPT;
+  SectionIterator(SectionIterator const& other) HADESMEM_NOEXCEPT
+    : impl_(other.impl_)
+  { }
 
-  SectionIterator& operator=(SectionIterator&& other) HADESMEM_NOEXCEPT;
+  SectionIterator& operator=(SectionIterator const& other) HADESMEM_NOEXCEPT
+  {
+    impl_ = other.impl_;
 
-  ~SectionIterator();
+    return *this;
+  }
+
+  SectionIterator(SectionIterator&& other) HADESMEM_NOEXCEPT
+    : impl_(std::move(other.impl_))
+  { }
+
+  SectionIterator& operator=(SectionIterator&& other) HADESMEM_NOEXCEPT
+  {
+    impl_ = std::move(other.impl_);
+
+    return *this;
+  }
+
+  ~SectionIterator()
+  { }
   
-  reference operator*() const HADESMEM_NOEXCEPT;
+  reference operator*() const HADESMEM_NOEXCEPT
+  {
+    HADESMEM_ASSERT(impl_.get());
+    return *impl_->section_;
+  }
   
-  pointer operator->() const HADESMEM_NOEXCEPT;
+  pointer operator->() const HADESMEM_NOEXCEPT
+  {
+    HADESMEM_ASSERT(impl_.get());
+    return &*impl_->section_;
+  }
   
-  SectionIterator& operator++();
+  SectionIterator& operator++()
+  {
+    HADESMEM_ASSERT(impl_.get());
+
+    WORD const number = impl_->section_->GetNumber();
+
+    NtHeaders const nt_headers(*impl_->process_, *impl_->pe_file_);
+    if (number + 1 >= nt_headers.GetNumberOfSections())
+    {
+      impl_.reset();
+      return *this;
+    }
+
+    PIMAGE_SECTION_HEADER new_base = reinterpret_cast<PIMAGE_SECTION_HEADER>(
+      impl_->section_->GetBase()) + 1;
+    impl_->section_ = Section(*impl_->process_, *impl_->pe_file_, 
+      static_cast<WORD>(number + 1), new_base);
   
-  SectionIterator operator++(int);
+    return *this;
+  }
   
-  bool operator==(SectionIterator const& other) const HADESMEM_NOEXCEPT;
+  SectionIterator operator++(int)
+  {
+    SectionIterator iter(*this);
+    ++*this;
+    return iter;
+  }
   
-  bool operator!=(SectionIterator const& other) const HADESMEM_NOEXCEPT;
+  bool operator==(SectionIterator const& other) const HADESMEM_NOEXCEPT
+  {
+    return impl_ == other.impl_;
+  }
+  
+  bool operator!=(SectionIterator const& other) const HADESMEM_NOEXCEPT
+  {
+    return !(*this == other);
+  }
   
 private:
+  struct Impl
+  {
+    explicit Impl(Process const& process, PeFile const& pe_file) 
+      HADESMEM_NOEXCEPT
+      : process_(&process), 
+      pe_file_(&pe_file), 
+      section_()
+    { }
+
+    Process const* process_;
+    PeFile const* pe_file_;
+    boost::optional<Section> section_;
+  };
+
   // Using a shared_ptr to provide shallow copy semantics, as 
   // required by InputIterator.
-  struct Impl;
   std::shared_ptr<Impl> impl_;
 };
 
@@ -62,29 +153,63 @@ public:
   typedef SectionIterator iterator;
   typedef SectionIterator const_iterator;
   
-  explicit SectionList(Process const& process, PeFile const& pe_file);
+  explicit SectionList(Process const& process, PeFile const& pe_file)
+    : process_(&process), 
+    pe_file_(&pe_file)
+  { }
 
-  SectionList(SectionList const& other);
+  SectionList(SectionList const& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    pe_file_(other.pe_file_)
+  { }
 
-  SectionList& operator=(SectionList const& other);
+  SectionList& operator=(SectionList const& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
 
-  SectionList(SectionList&& other) HADESMEM_NOEXCEPT;
+    return *this;
+  }
 
-  SectionList& operator=(SectionList&& other) HADESMEM_NOEXCEPT;
+  SectionList(SectionList&& other) HADESMEM_NOEXCEPT
+    : process_(other.process_), 
+    pe_file_(other.pe_file_)
+  { }
 
-  ~SectionList();
+  SectionList& operator=(SectionList&& other) HADESMEM_NOEXCEPT
+  {
+    process_ = other.process_;
+    pe_file_ = other.pe_file_;
+
+    return *this;
+  }
+
+  ~SectionList()
+  { }
   
-  iterator begin();
+  iterator begin()
+  {
+    return SectionList::iterator(*process_, *pe_file_);
+  }
   
-  const_iterator begin() const;
+  const_iterator begin() const
+  {
+    return SectionList::iterator(*process_, *pe_file_);
+  }
   
-  iterator end() HADESMEM_NOEXCEPT;
+  iterator end() HADESMEM_NOEXCEPT
+  {
+    return SectionList::iterator();
+  }
   
-  const_iterator end() const HADESMEM_NOEXCEPT;
+  const_iterator end() const HADESMEM_NOEXCEPT
+  {
+    return SectionList::iterator();
+  }
   
 private:
-  struct Impl;
-  std::unique_ptr<Impl> impl_;
+  Process const* process_;
+  PeFile const* pe_file_;
 };
 
 }
