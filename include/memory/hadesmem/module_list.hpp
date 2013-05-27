4c29a7ab97db9da271973089fd491d3180e68ad3
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-27 13:49:27 +0000

    * Inline ModuleList.

--- include/memory/hadesmem/module_list.hpp
+++ include/memory/hadesmem/module_list.hpp
@@ -11,8 +11,13 @@
 #include <boost/optional.hpp>
 #include <hadesmem/detail/warning_disable_suffix.hpp>
 
+#include <windows.h>
+#include <tlhelp32.h>
+
+#include <hadesmem/error.hpp>
 #include <hadesmem/config.hpp>
 #include <hadesmem/module.hpp>
+#include <hadesmem/process.hpp>
 #include <hadesmem/detail/assert.hpp>
 #include <hadesmem/detail/smart_handle.hpp>
 
@@ -32,7 +37,60 @@ public:
     : impl_()
   { }
   
-  explicit ModuleIterator(Process const& process);
+// TOOD: Clean this up.
+  explicit ModuleIterator(Process const& process)
+    : impl_(new Impl())
+  {
+    HADESMEM_ASSERT(impl_.get());
+  
+    impl_->process_ = &process;
+  
+    impl_->snap_ = detail::SmartHandle(
+      ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 
+      impl_->process_->GetId()), INVALID_HANDLE_VALUE);
+    if (!impl_->snap_.IsValid())
+    {
+      if (::GetLastError() == ERROR_BAD_LENGTH)
+      {
+        impl_->snap_ = detail::SmartHandle(
+          ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 
+          impl_->process_->GetId()), INVALID_HANDLE_VALUE);
+        if (!impl_->snap_.IsValid())
+        {
+          DWORD const last_error = ::GetLastError();
+          HADESMEM_THROW_EXCEPTION(Error() << 
+            ErrorString("CreateToolhelp32Snapshot failed.") << 
+            ErrorCodeWinLast(last_error));
+        }
+      }
+      else
+      {
+        DWORD const last_error = ::GetLastError();
+        HADESMEM_THROW_EXCEPTION(Error() << 
+          ErrorString("CreateToolhelp32Snapshot failed.") << 
+          ErrorCodeWinLast(last_error));
+      }
+    }
+  
+    MODULEENTRY32 entry;
+    ::ZeroMemory(&entry, sizeof(entry));
+    entry.dwSize = sizeof(entry);
+    if (!::Module32First(impl_->snap_.GetHandle(), &entry))
+    {
+      DWORD const last_error = ::GetLastError();
+      if (last_error == ERROR_NO_MORE_FILES)
+      {
+        impl_.reset();
+        return;
+      }
+    
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Module32First failed.") << 
+        ErrorCodeWinLast(last_error));
+    }
+  
+    impl_->module_ = Module(*impl_->process_, entry);
+  }
 
   ModuleIterator(ModuleIterator const& other) HADESMEM_NOEXCEPT
     : impl_(other.impl_)
@@ -71,7 +129,31 @@ public:
     return &*impl_->module_;
   }
 
-  ModuleIterator& operator++();
+  ModuleIterator& operator++()
+  {
+    HADESMEM_ASSERT(impl_.get());
+  
+    MODULEENTRY32 entry;
+    ::ZeroMemory(&entry, sizeof(entry));
+    entry.dwSize = sizeof(entry);
+    if (!::Module32Next(impl_->snap_.GetHandle(), &entry))
+    {
+      DWORD const last_error = ::GetLastError();
+      if (last_error == ERROR_NO_MORE_FILES)
+      {
+        impl_.reset();
+        return *this;
+      }
+    
+      HADESMEM_THROW_EXCEPTION(Error() << 
+        ErrorString("Module32Next failed.") << 
+        ErrorCodeWinLast(last_error));
+    }
+  
+    impl_->module_ = Module(*impl_->process_, entry);
+  
+    return *this;
+  }
   
   ModuleIterator operator++(int)
   {
