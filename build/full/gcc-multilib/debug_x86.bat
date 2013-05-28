c6ad184824484b544bd712cbacd3bb28260294ad
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-05-28 09:33:40 +0000

    * Build changes to make HadesMem header only. * Use dynamic instead of static runtime.

--- build/full/gcc-multilib/debug_x86.bat
+++ build/full/gcc-multilib/debug_x86.bat
@@ -1,7 +1,7 @@
 set OLDCD=%CD%
 pushd ..\..\..\
 set OLDPATH=%PATH%
-set PATH=%BOOST_ROOT%;%MINGW%;%MINGW%\bin\;%PATH%
+set PATH=%BOOST_ROOT%;%MINGW%;%MINGW%\bin\;%MINGW%\x86_64-w64-mingw32\lib32;%PATH%
 b2 -j %NUMBER_OF_PROCESSORS% toolset=gcc address-model=32 debug > %OLDCD%\debug_x86.txt 2>&1
 set PATH=%OLDPATH%
 popd
