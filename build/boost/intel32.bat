3f61104d5a12fd7a3554c9f69eda5ecd7eb5c2ca
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-06-11 12:21:32 +0000

    * Boost.Filesystem force enables the deprecated macro. Remove BOOST_FILESYSTEM_NO_DEPRECATED (also for System).

--- build/boost/intel32.bat
+++ build/boost/intel32.bat
@@ -1,4 +1,4 @@
 set OLDCD=%CD%
 pushd %BOOST_ROOT%
-b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=intel cxxflags=/Qstd=c++0x address-model=32 architecture=x86 --stagedir=stage/intel-x86 link=static runtime-link=shared threading=multi debug-symbols=on define=STRICT define=STRICT_TYPED_ITEMIDS define=UNICODE define=_UNICODE define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA define=BOOST_USE_WINDOWS_H define=BOOST_FILESYSTEM_NO_DEPRECATED define=BOOST_SYSTEM_NO_DEPRECATED stage > %OLDCD%\intel32.txt 2>&1
+b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=intel cxxflags=/Qstd=c++0x address-model=32 architecture=x86 --stagedir=stage/intel-x86 link=static runtime-link=shared threading=multi debug-symbols=on define=STRICT define=STRICT_TYPED_ITEMIDS define=UNICODE define=_UNICODE define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA define=BOOST_USE_WINDOWS_H stage > %OLDCD%\intel32.txt 2>&1
 popd
--- build/boost/msvc64.bat
+++ build/boost/msvc64.bat
@@ -1,4 +1,4 @@
 set OLDCD=%CD%
 pushd %BOOST_ROOT%
-b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=msvc address-model=64 architecture=x86 --stagedir=stage/msvc-x64 link=static runtime-link=shared threading=multi debug-symbols=on define=STRICT define=STRICT_TYPED_ITEMIDS define=UNICODE define=_UNICODE define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA define=BOOST_USE_WINDOWS_H define=BOOST_FILESYSTEM_NO_DEPRECATED define=BOOST_SYSTEM_NO_DEPRECATED stage > %OLDCD%\msvc64.txt 2>&1
+b2 -j %NUMBER_OF_PROCESSORS% --without-python toolset=msvc address-model=64 architecture=x86 --stagedir=stage/msvc-x64 link=static runtime-link=shared threading=multi debug-symbols=on define=STRICT define=STRICT_TYPED_ITEMIDS define=UNICODE define=_UNICODE define=WINVER=_WIN32_WINNT_VISTA define=_WIN32_WINNT=_WIN32_WINNT_VISTA define=BOOST_USE_WINDOWS_H stage > %OLDCD%\msvc64.txt 2>&1
 popd
