588d4efb3688c913be35e623d5997d8d39effea1
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-07-23 23:53:53 +0000

    * Fix GCC crash. Base hook test seems to be working on all platforms on my two text binaries (CS2D for x86, and SNES-9x for x64).

--- include/memory/hadesmem/detail/initialize.hpp
+++ include/memory/hadesmem/detail/initialize.hpp
@@ -313,9 +313,9 @@ inline void InitializeAll()
   EnableCrtDebugFlags();
   EnableTerminationOnHeapCorruption();
   EnableBottomUpRand();
-  // Enabling mitigation policies under Clang causes a crash.
-  // TODO: Investigate why this is failing under Clang and fix.
-#if !defined(HADESMEM_CLANG)
+  // Enabling mitigation policies under MinGW-w64 causes a crash.
+  // TODO: Investigate why this is failing under MinGW-w64 and fix.
+#if !defined(HADESMEM_CLANG) && !defined(HADESMEM_GCC)
   EnableMitigationPolicies();
 #endif
   ImbueAllDefault();
