75493352d0cf20a53e14ae5b6403a5e8abfc438c
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2013-12-29 08:59:28 +0000

    * [Dump] Fix for previous commit. (Moved the comments but forgot to hit save.)

--- examples/dump/sections.cpp
+++ examples/dump/sections.cpp
@@ -11,12 +11,14 @@
 #include <hadesmem/pelib/pe_file.hpp>
 #include <hadesmem/process.hpp>
 
+// TODO: Detect when a file has no sections.
+
+// TODO: Detect when a file has more than 96 sections (allowed on Vista+, but
+// technically outside of the spec).
+
 void DumpSections(hadesmem::Process const& process,
                   hadesmem::PeFile const& pe_file)
 {
-  // TODO: Detect when a file has no sections.
-  // TODO: Detect when a file has more than 96 sections (allowed on Vista+, but
-  // technically outside of the spec).
   hadesmem::SectionList sections(process, pe_file);
 
   if (std::begin(sections) != std::end(sections))
