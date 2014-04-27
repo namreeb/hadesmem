116edaf791d5dbd6fc4f9dbe8e27e97699271cbd
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2014-04-27 20:15:42 +0000

    * [Cerberus] Cleanup.

--- examples/cerberus/main.cpp
+++ examples/cerberus/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (C) 2010-2014 Joshua Boyce.
 // See the file COPYING for copying permission.
 
+#include "main.hpp"
+
 #include <windows.h>
 
 #include <hadesmem/config.hpp>
@@ -19,6 +21,52 @@
 
 // WARNING! Most of this is untested, it's for expository and testing
 // purposes only.
+
+namespace
+{
+
+bool IsSafeToUnload()
+{
+  auto const& process = GetThisProcess();
+  bool safe = true;
+  std::size_t retries = 5;
+  do
+  {
+    hadesmem::SuspendedProcess suspend{process.GetId()};
+    hadesmem::ThreadList threads{process.GetId()};
+    safe = true;
+    for (auto const& thread_entry : threads)
+    {
+      auto const id = thread_entry.GetId();
+      if (id == ::GetCurrentThreadId())
+      {
+        continue;
+      }
+
+      hadesmem::Thread const thread{id};
+      auto const context = GetThreadContext(thread, CONTEXT_CONTROL);
+#if defined(HADESMEM_DETAIL_ARCH_X64)
+      auto const ip = reinterpret_cast<void const*>(context.Rip);
+#elif defined(HADESMEM_DETAIL_ARCH_X86)
+      auto const ip = reinterpret_cast<void const*>(context.Eip);
+#else
+#error "[HadesMem] Unsupported architecture."
+#endif
+      HADESMEM_DETAIL_ASSERT(ip);
+      auto const this_module =
+        reinterpret_cast<std::uint8_t*>(hadesmem::detail::GetHandleToSelf());
+      auto const this_module_size = hadesmem::detail::GetRegionAllocSize(
+        process, reinterpret_cast<void const*>(this_module));
+      if (ip >= this_module && ip < this_module + this_module_size)
+      {
+        safe = false;
+      }
+    }
+  } while (!safe && retries--);
+
+  return safe;
+}
+}
 
 hadesmem::Process& GetThisProcess()
 {
@@ -58,47 +106,6 @@ extern "C" HADESMEM_DETAIL_DLLEXPORT DWORD_PTR Free() HADESMEM_DETAIL_NOEXCEPT
 {
   try
   {
-    auto const& process = GetThisProcess();
-    bool safe = true;
-    std::size_t retries = 5;
-    do
-    {
-      hadesmem::SuspendedProcess suspend{ process.GetId() };
-      hadesmem::ThreadList threads{ process.GetId() };
-      safe = true;
-      for (auto const& thread_entry : threads)
-      {
-        auto const id = thread_entry.GetId();
-        if (id == ::GetCurrentThreadId())
-        {
-          continue;
-        }
-
-        hadesmem::Thread const thread{id};
-        auto const context = GetThreadContext(thread, CONTEXT_CONTROL);
-#if defined(HADESMEM_DETAIL_ARCH_X64)
-        auto const ip = reinterpret_cast<void const*>(context.Rip);
-#elif defined(HADESMEM_DETAIL_ARCH_X86)
-        auto const ip = reinterpret_cast<void const*>(context.Eip);
-#else
-#error "[HadesMem] Unsupported architecture."
-#endif
-        HADESMEM_DETAIL_ASSERT(ip);
-        auto const this_module = reinterpret_cast<std::uint8_t*>(hadesmem::detail::GetHandleToSelf());
-        auto const this_module_size = hadesmem::detail::GetRegionAllocSize(
-          process, reinterpret_cast<void const*>(this_module));
-        if (ip >= this_module && ip < this_module + this_module_size)
-        {
-          safe = false;
-        }
-      }
-    } while (!safe && retries--);
-
-    if (!safe)
-    {
-      return 2;
-    }
-
     UndetourNtQuerySystemInformation();
     UndetourNtCreateUserProcess();
     UndetourNtQueryDirectoryFile();
@@ -107,6 +114,11 @@ extern "C" HADESMEM_DETAIL_DLLEXPORT DWORD_PTR Free() HADESMEM_DETAIL_NOEXCEPT
 
     UndetourDXGI(true);
     UndetourD3D11(true);
+
+    if (!IsSafeToUnload())
+    {
+      return 2;
+    }
 
     return 0;
   }
