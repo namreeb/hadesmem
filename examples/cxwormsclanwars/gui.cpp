4591426c1e1a3efd1416c5626e9c190cb31c6e29
raptorfactor@raptorfactor.com <raptorfactor@raptorfactor.com@cdac023c-1092-11df-8e59-a5c54279f3f5>
2014-10-30 15:21:56 +0000

    * {CXWormsClanWars] Add placeholder ext.

---
+++ examples/cxwormsclanwars/gui.cpp
@@ -0,0 +1,118 @@
+// Copyright (C) 2010-2014 Joshua Boyce.
+// See the file COPYING for copying permission.
+
+#include "gui.hpp"
+
+#include <cstddef>
+#include <cstdint>
+
+#include <hadesmem/config.hpp>
+#include <hadesmem/detail/trace.hpp>
+#include <hadesmem/error.hpp>
+
+namespace
+{
+std::size_t g_on_ant_tweak_bar_initialize_callback_id =
+  static_cast<std::size_t>(-1);
+
+std::size_t g_on_ant_tweak_bar_cleanup_callback_id =
+  static_cast<std::size_t>(-1);
+
+TwBar* g_tweak_bar = nullptr;
+
+void TW_CALL PlaceholderCallbackTw(void* /*client_data*/)
+{
+  HADESMEM_DETAIL_TRACE_A("Called.");
+}
+
+void OnAntTweakBarInitialize(
+  hadesmem::cerberus::AntTweakBarInterface* ant_tweak_bar)
+{
+  HADESMEM_DETAIL_TRACE_A("Initializing AntTweakBar.");
+
+  if (g_tweak_bar)
+  {
+    HADESMEM_DETAIL_TRACE_A(
+      "WARNING! AntTweakBar is already initialized. Skipping.");
+    return;
+  }
+
+  if (!ant_tweak_bar->IsInitialized())
+  {
+    HADESMEM_DETAIL_TRACE_A(
+      "WARNING! AntTweakBar is not initialized by Cerberus. Skipping.");
+    return;
+  }
+
+  g_tweak_bar = ant_tweak_bar->TwNewBar("CXWormsClanWars");
+  if (!g_tweak_bar)
+  {
+    HADESMEM_DETAIL_THROW_EXCEPTION(
+      hadesmem::Error{} << hadesmem::ErrorString{"TwNewBar failed."}
+                        << hadesmem::ErrorStringOther{TwGetLastError()});
+  }
+
+  auto const placeholder_button =
+    ant_tweak_bar->TwAddButton(g_tweak_bar,
+                               "CXWormsClanWars_PlaceholderBtn",
+                               &PlaceholderCallbackTw,
+                               nullptr,
+                               " label='Placeholder' ");
+  if (!placeholder_button)
+  {
+    HADESMEM_DETAIL_THROW_EXCEPTION(
+      hadesmem::Error{} << hadesmem::ErrorString{"TwAddButton failed."}
+                        << hadesmem::ErrorStringOther{TwGetLastError()});
+  }
+}
+
+void
+  OnAntTweakBarCleanup(hadesmem::cerberus::AntTweakBarInterface* ant_tweak_bar)
+{
+  HADESMEM_DETAIL_TRACE_A("Cleaning up AntTweakBar.");
+
+  if (!ant_tweak_bar->IsInitialized())
+  {
+    HADESMEM_DETAIL_TRACE_A(
+      "WARNING! AntTweakBar is not initialized by Cerberus. Skipping.");
+    return;
+  }
+
+  if (g_tweak_bar != nullptr)
+  {
+    ant_tweak_bar->TwDeleteBar(g_tweak_bar);
+    g_tweak_bar = nullptr;
+  }
+}
+}
+
+void InitializeGui(hadesmem::cerberus::PluginInterface* cerberus)
+{
+  g_on_ant_tweak_bar_initialize_callback_id =
+    cerberus->GetRenderInterface()->RegisterOnAntTweakBarInitialize(
+      &OnAntTweakBarInitialize);
+  g_on_ant_tweak_bar_cleanup_callback_id =
+    cerberus->GetRenderInterface()->RegisterOnAntTweakBarCleanup(
+      &OnAntTweakBarCleanup);
+
+  OnAntTweakBarInitialize(
+    cerberus->GetRenderInterface()->GetAntTweakBarInterface());
+}
+
+void CleanupGui(hadesmem::cerberus::PluginInterface* cerberus)
+{
+  if (g_on_ant_tweak_bar_initialize_callback_id != static_cast<std::size_t>(-1))
+  {
+    cerberus->GetRenderInterface()->UnregisterOnAntTweakBarInitialize(
+      g_on_ant_tweak_bar_initialize_callback_id);
+  }
+
+  if (g_on_ant_tweak_bar_cleanup_callback_id != static_cast<std::size_t>(-1))
+  {
+    cerberus->GetRenderInterface()->UnregisterOnAntTweakBarCleanup(
+      g_on_ant_tweak_bar_cleanup_callback_id);
+  }
+
+  OnAntTweakBarCleanup(
+    cerberus->GetRenderInterface()->GetAntTweakBarInterface());
+}
---
+++ examples/cxwormsclanwars/gui.hpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2010-2014 Joshua Boyce.
+// See the file COPYING for copying permission.
+
+#pragma once
+
+#include "../cerberus/plugin.hpp"
+
+void InitializeGui(hadesmem::cerberus::PluginInterface* cerberus);
+
+void CleanupGui(hadesmem::cerberus::PluginInterface* cerberus);
