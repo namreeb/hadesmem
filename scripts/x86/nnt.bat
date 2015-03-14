280722fde0c0555781d554b50c66be4d90cb3afa
Joshua Boyce <raptorfactor@raptorfactor.com>
2015-03-15 05:18:42 +1100

    [Scripts] Add Oddworld script.

---
+++ scripts/x86/nnt.bat
@@ -0,0 +1,1 @@
+inject.exe --run "C:\Program Files (x86)\Steam\SteamApps\common\Oddworld New n Tasty\NNT.exe" --inject --steam-app-id 314660 --export Load --add-path --path-resolution --module cerberus.dll
