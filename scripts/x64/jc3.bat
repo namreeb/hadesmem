093594e9fa8101e6f6bf4d4d34ec15c22f364b87
Joshua Boyce <raptorfactor@raptorfactor.com>
2015-12-16 05:37:30 +1100

    [build] Update gitignore. [scripts] Add JC3.

---
+++ scripts/x64/jc3.bat
@@ -0,0 +1,1 @@
+inject.exe --run "C:\Program Files (x86)\Steam\steamapps\common\Just Cause 3\JustCause3.exe" --inject --steam-app-id 225540 --export Load --add-path --path-resolution --module cerberus.dll
