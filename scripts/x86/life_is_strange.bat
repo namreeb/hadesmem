63439fa67a84ad3de4025c4103ac45d2f829f978
Joshua Boyce <raptorfactor@raptorfactor.com>
2016-08-23 22:09:06 -0700

    * Add script for Life is Strange.

---
+++ scripts/x86/life_is_strange.bat
@@ -0,0 +1,1 @@
+inject.exe --run "G:\Jeux\Steam\steamapps\common\Life Is Strange\Binaries\Win32\LifeIsStrange.exe" --inject --steam-app-id 319630 --export Load --add-path --path-resolution --module cerberus.dll
